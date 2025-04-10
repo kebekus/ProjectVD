be7eb59eddcdc3607bccde66b0908f9aeee785ac
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-04-10 10:40:45 +0200

    Update LaurentPolynomial2.lean

--- VD/LaurentPolynomial2.lean
+++ VD/LaurentPolynomial2.lean
@@ -9,16 +9,16 @@ import Mathlib.Analysis.Meromorphic.NormalForm
 /-!
 # Factorized Rational Functions
 
-This file discusses functions `𝕜 → 𝕜` of the form `∏ᶠ u, (· - u) ^ d u`,
-where `d : 𝕜 → ℤ`. We show that these "factorized rational functions" are
-meromorphic in normal form, with divisor equal to `d`.
+This file discusses functions `𝕜 → 𝕜` of the form `∏ᶠ u, (· - u) ^ d u`, where
+`d : 𝕜 → ℤ` is integer-valued. We show that these "factorized rational
+functions" are meromorphic in normal form, with divisor equal to `d`.
 
 TODO: Show that every meromorphic functions on a compact set is equivalent,
 modulo equality on codiscrete sets, the the product of a factorized rational
 function and an analytic function without zeros.
 -/
 
-open Classical Function
+open Function
 
 variable
   {𝕜 : Type*} [NontriviallyNormedField 𝕜]
@@ -27,16 +27,10 @@ variable
   {z : 𝕜}
 
 /--
-Factorized rational functions on a non-trivially normed field `𝕜` are functions
-of the form `∏ᶠ u, (· - u) ^ d u`.
--/
-noncomputable abbrev Function.FactorizedRational (d : 𝕜 → ℤ) := ∏ᶠ u, (· - u) ^ d u
-
-/--
 Helper Lemma: Identify the support of `d` as the mulsupport of the product
 defining the factorized rational function.
 -/
-lemma Function.mulSupport_factorizedRational (d : 𝕜 → ℤ) :
+lemma Function.FactorizedRational.mulSupport (d : 𝕜 → ℤ) :
     (Function.mulSupport fun u ↦ (· - u) ^ d u) = d.support := by
   ext u
   constructor
@@ -57,7 +51,7 @@ lemma Function.mulSupport_factorizedRational (d : 𝕜 → ℤ) :
 lemma Function.FactorizedRational.analyticAt {d : 𝕜 → ℤ} {x : 𝕜} (h : 0 ≤ d x) :
     AnalyticAt 𝕜 (∏ᶠ u, (· - u) ^ d u) x := by
   by_cases h₁ : Set.Finite d.support
-  · rw [← Function.mulSupport_factorizedRational d] at h₁
+  · rw [← Function.FactorizedRational.mulSupport d] at h₁
     rw [finprod_eq_prod _ h₁]
     have : (∏ i ∈ h₁.toFinset, (fun x ↦ x - i) ^ d i) = (fun x ↦ ∏ i ∈ h₁.toFinset, (x - i) ^ d i) := by
       ext x
@@ -72,14 +66,14 @@ lemma Function.FactorizedRational.analyticAt {d : 𝕜 → ℤ} {x : 𝕜} (h : 
     · apply AnalyticAt.fun_zpow
       fun_prop
       rwa [sub_ne_zero]
-  · rw [← Function.mulSupport_factorizedRational d] at h₁
+  · rw [← Function.FactorizedRational.mulSupport d] at h₁
     rw [finprod_of_infinite_mulSupport h₁]
     apply analyticAt_const
 
 lemma Function.FactorizedRational.zeroAt {d : 𝕜 → ℤ} {x : 𝕜} (h : d x = 0) :
     (∏ᶠ u, (· - u) ^ d u) x ≠ 0 := by
   by_cases h₁ : Set.Finite d.support
-  · rw [← Function.mulSupport_factorizedRational d] at h₁
+  · rw [← Function.FactorizedRational.mulSupport d] at h₁
     rw [finprod_eq_prod _ h₁]
     rw [Finset.prod_apply]
     rw [Finset.prod_ne_zero_iff]
@@ -90,24 +84,25 @@ lemma Function.FactorizedRational.zeroAt {d : 𝕜 → ℤ} {x : 𝕜} (h : d x 
     · apply zpow_ne_zero
       rw [sub_ne_zero]
       tauto
-  · rw [← Function.mulSupport_factorizedRational d] at h₁
+  · rw [← Function.FactorizedRational.mulSupport d] at h₁
     rw [finprod_of_infinite_mulSupport h₁]
     simp
 
+open Classical in
 lemma Function.FactorizedRational.analyticAt' {d : 𝕜 → ℤ} (u₀ : 𝕜) (hd : d.support.Finite) :
     (∏ᶠ u, (· - u) ^ d u) = ((· - u₀) ^ d u₀) * (∏ᶠ u, (· - u) ^ (update d u₀ 0 u)) := by
   by_cases h₁d : d u₀ = 0
   · rw [← eq_update_self_iff.2 h₁d]
     simp [h₁d]
   have t₀ : (mulSupport fun u ↦ (fun x ↦ x - u) ^ d u) ⊆ hd.toFinset := by
-    simp [Function.mulSupport_factorizedRational]
+    simp [Function.FactorizedRational.mulSupport]
   rw [finprod_eq_prod_of_mulSupport_subset _ t₀]
   have t₁ : u₀ ∈ hd.toFinset := by
     simp_all
   rw [← Finset.mul_prod_erase hd.toFinset _ t₁]
   congr 1
   have t₂ : (mulSupport fun u ↦ (fun x ↦ x - u) ^ (update d u₀ 0 u)) ⊆ hd.toFinset.erase u₀ := by
-    rw [Function.mulSupport_factorizedRational]
+    rw [Function.FactorizedRational.mulSupport]
     intro x hx
     by_cases h₁x : x = u₀ <;> simp_all
   rw [finprod_eq_prod_of_mulSupport_subset _ t₂]
@@ -115,16 +110,6 @@ lemma Function.FactorizedRational.analyticAt' {d : 𝕜 → ℤ} (u₀ : 𝕜) (
   intro x hx
   rw [eq_comm]
   simp_all
-
-private lemma analyticAt_finLaurentPolynomial_off_supportx (d : 𝕜 → ℤ) (P : Finset 𝕜)
-    (hz : z ∉ P) :
-    AnalyticAt 𝕜 (∏ u ∈ P, fun z ↦ (z - u) ^ d u) z := by
-  rw [Finset.prod_fn]
-  apply Finset.analyticAt_prod
-  intro u hu
-  apply AnalyticAt.zpow (by fun_prop)
-  rw [sub_ne_zero, ne_comm]
-  exact ne_of_mem_of_not_mem hu hz
 
 /-- Laurent polynomials are meromorphic in normal form on `⊤`. -/
 theorem meromorphicNFOn_laurentPolynomial_top (d : 𝕜 → ℤ) :
@@ -141,7 +126,7 @@ theorem meromorphicNFOn_laurentPolynomial_top (d : 𝕜 → ℤ) :
       · apply FactorizedRational.zeroAt
         simp
       · simp
-  · rw [← Function.mulSupport_factorizedRational d] at hd
+  · rw [← Function.FactorizedRational.mulSupport d] at hd
     rw [finprod_of_infinite_mulSupport hd]
     apply AnalyticOnNhd.meromorphicNFOn
     apply analyticOnNhd_const
@@ -149,7 +134,7 @@ theorem meromorphicNFOn_laurentPolynomial_top (d : 𝕜 → ℤ) :
 
 /-- Laurent polynomials are meromorphic in normal form on arbitrary subsets of `𝕜`. -/
 theorem meromorphicNFOn_laurentPolynomial (d : 𝕜 → ℤ) (U : Set 𝕜) :
-    MeromorphicNFOn (Function.FactorizedRational d) U := by
+    MeromorphicNFOn (∏ᶠ u, (· - u) ^ d u) U := by
   intro z hz
   exact meromorphicNFOn_laurentPolynomial_top d (trivial)
 
@@ -176,22 +161,10 @@ theorem order_LaurentPolynomial_ne_top {z : 𝕜} (d : 𝕜 → ℤ) :
     (meromorphicNFOn_laurentPolynomial_top d (trivial : z ∈ ⊤)).meromorphicAt.order ≠ ⊤ := by
   by_cases hd : Set.Finite (Function.support d)
   · simp [order_laurentPolynomial d hd]
-  · rw [← Function.mulSupport_factorizedRational] at hd
+  · rw [← Function.FactorizedRational.mulSupport] at hd
     have : AnalyticAt 𝕜 (1 : 𝕜 → 𝕜) z := analyticAt_const
     simp [finprod_of_infinite_mulSupport hd, this.meromorphicAt_order,
       this.order_eq_zero_iff.2 (by simp)]
-
-/--
-The divisor function associated with the divisor of the Laurent polynomial
-`(∏ᶠ u, fun z ↦ (z - u) ^ d u)` equals `d`.
--/
-theorem divisor_LaurentPolynomial [CompleteSpace 𝕜] (d : 𝕜 → ℤ) (h₁d : Set.Finite d.support) :
-    MeromorphicOn.divisor (Function.FactorizedRational d) ⊤ = d := by
-  ext z
-  simp_rw [(meromorphicNFOn_laurentPolynomial_top d).meromorphicOn.divisor_apply
-    (by simp : z ∈ Set.univ)]
-  rw [order_laurentPolynomial d h₁d]
-  simp
 
 /--
 If `D` is a divisor, then the function associated with the divisor of the Laurent polynomial equals
@@ -199,7 +172,7 @@ If `D` is a divisor, then the function associated with the divisor of the Lauren
 -/
 theorem divisor_laurentPolynomial_within [CompleteSpace 𝕜] {U : Set 𝕜}
     (D : Function.locallyFinsuppWithin U ℤ) (hD : Set.Finite D.support) :
-    MeromorphicOn.divisor (Function.FactorizedRational D) U = D := by
+    MeromorphicOn.divisor (∏ᶠ u, (· - u) ^ D u) U = D := by
   ext z
   by_cases hz : z ∈ U
   · simp [(meromorphicNFOn_laurentPolynomial D U).meromorphicOn, hz,
