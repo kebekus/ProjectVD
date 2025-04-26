fe9154318e88572dbcf75e772ef1896e7d88c8b2
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-04-26 17:03:42 +0200

    Update LeadCoefficient.lean

--- VD/LeadCoefficient.lean
+++ VD/LeadCoefficient.lean
@@ -1,12 +1,15 @@
 import Mathlib.Analysis.Meromorphic.Order
+import VD.ToMathlib.MeromorphicAt_order
 
 variable
   {𝕜 : Type*} [NontriviallyNormedField 𝕜]
   {E : Type u_2} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
   {f : 𝕜 → E} {x : 𝕜}
 
-
 noncomputable def leadCoefficient (f : 𝕜 → E) (x : 𝕜) : E := by
-  by_cases h : MeromorphicAt f x
+  by_cases h₁ : ¬MeromorphicAt f x
   · exact 0
+  rw [not_not] at h₁
+  by_cases h₂ : h₁.order = ⊤
   · exact 0
+  exact (h₁.order_ne_top_iff.1 h₂).choose x
