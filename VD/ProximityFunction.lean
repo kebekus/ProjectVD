3a253336858d936ad882e61d82ed75718f19b27e
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-05-14 08:34:36 +0200

    Proximity function

--- VD/ProximityFunction.lean
+++ VD/ProximityFunction.lean
@@ -3,10 +3,10 @@ Copyright (c) 2025 Stefan Kebekus. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Stefan Kebekus
 -/
-import Mathlib.Analysis.Meromorphic.Divisor
 import Mathlib.Analysis.SpecialFunctions.Log.PosLog
 import Mathlib.MeasureTheory.Integral.CircleAverage
 import VD.ToMathlib.meromorphicOn_integrability
+import VD.ToMathlib.ProximityFunction
 
 /-!
 # Circle Averages
@@ -27,95 +27,29 @@ theorem circleAverage_sub (hf₁ : CircleIntegrable f₁ c R) (hf₂ : CircleInt
   apply intervalIntegral.integral_sub hf₁ hf₂
 
 
-/-!
-# The Proximity Function of Value Distribution Theory
--/
-
 open MeromorphicOn Metric Real Set
 
 namespace ValueDistribution
 
 variable
-  {E : Type*} [NormedAddCommGroup E] [NormedSpace ℂ E]
-  {f g : ℂ → ℂ} {a : WithTop ℂ} {a₀ : ℂ}
+  {E : Type*} [NormedAddCommGroup E]
+  {f g : ℂ → E} {a : WithTop E} {a₀ : E}
 
 open Real
 
-variable (f a) in
-/--
-The proximity function of a meromorphic function.
-
-If `f : ℂ → ℂ` is meromorphic and `a : WithTop ℂ` is any value, this is a logarithmically weighted
-measure of the number of times the function `f` takes a given value `a` within the disk `∣z∣ ≤ r`,
-counting multiplicities.  In the special case where `a = ⊤`, it counts the poles of `f`.
--/
-noncomputable def proximity : ℝ → ℝ := by
-  by_cases h : a = ⊤
-  · exact circleAverage (log⁺ ‖f ·‖) 0
-  · exact circleAverage (fun z ↦ log⁺ ‖1 / (f z - a.untop₀)‖) 0
 
 /--
-For finite values `a₀`, the logarithmic counting function `logCounting f a₀` is the counting
-function associated with the zero-divisor of the meromorphic function `f - a₀`.
+For complex-valued `f`, the difference between `proximity f ⊤` and `proximity
+f⁻¹ ⊤` is the circle average of `log ‖f ·‖`.
 -/
-lemma proximity_coe :
-    proximity f a₀ = circleAverage (fun z ↦ log⁺ ‖1 / (f z - a₀)‖) 0 := by
-  simp [proximity]
-
-/--
-The logarithmic counting function `logCounting f 0` is the counting function associated with the
-zero-divisor of `f`.
--/
-lemma proximity_zero :
-    proximity f 0 = circleAverage (log⁺ ‖f⁻¹ ·‖) 0 := by
-  simp [proximity]
-
-/--
-The logarithmic counting function `logCounting f ⊤` is the counting function associated with
-the pole-divisor of `f`.
--/
-lemma proximity_top :
-    proximity f ⊤ = circleAverage (log⁺ ‖f ·‖) 0 := by
-  simp [proximity]
-
-/-!
-## Elementary Properties of the Counting Function
--/
-
-/--
-For finite values `a₀`, the logarithmic counting function `logCounting f a₀` equals the logarithmic
-counting function for the value zero of the shifted function `f - a₀`.
--/
-lemma proximity_coe_eq_proximity_sub_const_zero :
-    proximity f a₀ = proximity (f - fun _ ↦ a₀) 0 := by
-  simp [proximity]
-
-/--
-The counting function associated with the divisor of `f` is the difference between `logCounting f 0`
-and `logCounting f ⊤`.
--/
-theorem Nevanlinna_proximity (h₁f : MeromorphicOn f ⊤) :
-  circleAverage (log ‖f ·‖) 0 = proximity f ⊤ - proximity f⁻¹ ⊤ := by
+theorem proximity_sub_proximity_inv_eq_circleAverage {f : ℂ → ℂ} (h₁f : MeromorphicOn f ⊤) :
+    proximity f ⊤ - proximity f⁻¹ ⊤ = circleAverage (log ‖f ·‖) 0 := by
   ext R
-  simp only [proximity, ↓reduceDIte, Pi.inv_apply, norm_inv, Pi.sub_apply]
-  rw [← circleAverage_sub]
-  congr
-  funext x
-  simp [← posLog_sub_posLog_inv]
-  --
-  apply MeromorphicOn.circleIntegrable_posLog_norm
-  intro z hx
-  exact h₁f z trivial
-  --
-  simp_rw [← norm_inv]
-  apply MeromorphicOn.circleIntegrable_posLog_norm
-  exact MeromorphicOn.inv_iff.mpr fun x a => h₁f x trivial
-
-/--
-Relation between the logarithmic counting function of `f` and of `f⁻¹`.
--/
-@[simp] theorem proximity_inv :
-     proximity f⁻¹ ⊤ = proximity f 0 := by
-  simp [proximity_zero, proximity_top]
+  have i₀ : CircleIntegrable (log⁺ ‖f ·‖) 0 R :=
+    (h₁f.mono_set (by tauto)).circleIntegrable_posLog_norm
+  have i₁ : CircleIntegrable (fun x ↦ log⁺ ‖f x‖⁻¹) 0 R := by
+    simpa [← norm_inv] using (h₁f.inv.mono_set (by tauto)).circleIntegrable_posLog_norm
+  simp [proximity, ← circleAverage_sub i₀ i₁, ← posLog_sub_posLog_inv]
+  rfl
 
 end ValueDistribution
