9e89501cb2dfb3cb7f9079263b6c87a1145b9efc
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-05-27 16:35:43 +0200

    Working…

---
+++ VD/IteratedFDeriv_two.lean
@@ -0,0 +1,39 @@
+import Mathlib.Analysis.Calculus.ContDiff.FTaylorSeries
+
+open TensorProduct
+
+variable
+  {𝕜 : Type*} [NontriviallyNormedField 𝕜]
+  {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+  {F : Type*} [NormedAddCommGroup F] [NormedSpace 𝕜 F]
+
+variable (𝕜) in
+/--
+Convenience reformulation of the second iterated derivative, as a map from `E`
+to bilinear maps `E →ₗ[ℝ] E →ₗ[ℝ] ℝ
+-/
+noncomputable def bilinear_of_iteratedFDeriv_two (f : E → F) : E → E →ₗ[𝕜] E →ₗ[𝕜] F :=
+  fun x ↦ (fderiv 𝕜 (fderiv 𝕜 f) x).toLinearMap₂
+
+/--
+Expression of `bilinear_of_iteratedFDeriv_two` in terms of `iteratedFDeriv`.
+-/
+lemma bilinear_of_iteratedFDeriv_two_eq_iteratedFDeriv (f : E → F) (e e₁ e₂ : E) :
+    bilinear_of_iteratedFDeriv_two 𝕜 f e e₁ e₂ = iteratedFDeriv 𝕜 2 f e ![e₁, e₂] := by
+  simp [iteratedFDeriv_two_apply f e ![e₁, e₂], bilinear_of_iteratedFDeriv_two]
+
+variable (𝕜) in
+/--
+Convenience reformulation of the second iterated derivative, as a map from `E`
+to linear maps `E ⊗[𝕜] E →ₗ[𝕜] F`.
+-/
+noncomputable def tensor_of_iteratedFDeriv_two (f : E → F) : E → E ⊗[𝕜] E →ₗ[𝕜] F :=
+  fun e ↦ lift (bilinear_of_iteratedFDeriv_two 𝕜 f e)
+
+/--
+Expression of `tensor_of_iteratedFDeriv_two` in terms of `iteratedFDeriv`.
+-/
+lemma tensor_of_iteratedFDeriv_two_eq_iteratedFDeriv (f : E → F) (e e₁ e₂ : E) :
+    tensor_of_iteratedFDeriv_two 𝕜 f e (e₁ ⊗ₜ[𝕜] e₂) = iteratedFDeriv 𝕜 2 f e ![e₁, e₂] := by
+  rw [← bilinear_of_iteratedFDeriv_two_eq_iteratedFDeriv, tensor_of_iteratedFDeriv_two]
+  rfl
--- VD/Laplace2.lean
+++ VD/Laplace2.lean
@@ -1,7 +1,45 @@
-import Mathlib.Analysis.Complex.Basic
-import VD.partialDeriv
+import Mathlib.Analysis.InnerProductSpace.PiL2
+--import Mathlib.Analysis.InnerProductSpace.CanonicalTensor
+import VD.IteratedFDeriv_two
 
-variable {F G : Type*} [NormedAddCommGroup F] [NormedSpace ℝ F] [NormedAddCommGroup G] [NormedSpace ℝ G]
+open InnerProductSpace TensorProduct
 
-noncomputable def Complex.laplace2 (f : ℂ → F) : ℂ → F :=
-  partialDeriv ℝ 1 (partialDeriv ℝ 1 f) + partialDeriv ℝ Complex.I (partialDeriv ℝ Complex.I f)
+variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E] [FiniteDimensional ℝ E]
+
+
+variable (E) in
+/--
+The canonical covariant tensor corresponding to `InnerProductSpace.canonicalContravariantTensor`
+under the identification of `E` with its dual.
+-/
+noncomputable def InnerProductSpace.canonicalCovariantTensor :
+    E ⊗[ℝ] E := ∑ i, ((stdOrthonormalBasis ℝ E) i) ⊗ₜ[ℝ] ((stdOrthonormalBasis ℝ E) i)
+
+/-- Representation of the canonical covariant tensor in terms of an orthonormal basis. -/
+theorem InnerProductSpace.canonicalCovariantTensor_eq_sum
+    {ι : Type*} [Fintype ι] (v : OrthonormalBasis ι ℝ E) :
+    InnerProductSpace.canonicalCovariantTensor E = ∑ i, (v i) ⊗ₜ[ℝ] (v i) := by
+  let w := stdOrthonormalBasis ℝ E
+  calc ∑ m, w m ⊗ₜ[ℝ] w m
+  _ = ∑ m, ∑ n, ⟪w m, w n⟫_ℝ • w m ⊗ₜ[ℝ] w n := by
+    congr 1 with m
+    rw [Fintype.sum_eq_single m _, orthonormal_iff_ite.1 w.orthonormal]
+    · simp only [↓reduceIte, one_smul]
+    simp only [orthonormal_iff_ite.1 w.orthonormal, ite_smul, one_smul, zero_smul,
+      ite_eq_right_iff]
+    tauto
+  _ = ∑ m, ∑ n, (∑ i, ⟪w m, v i⟫_ℝ * ⟪v i, w n⟫_ℝ) • w m ⊗ₜ[ℝ] w n := by
+    simp_rw [OrthonormalBasis.sum_inner_mul_inner v]
+  _ = ∑ m, ∑ n, (∑ i, ⟪w m, v i⟫_ℝ * ⟪w n, v i⟫_ℝ) • w m ⊗ₜ[ℝ] w n := by
+    simp only [real_inner_comm (w _)]
+  _ = ∑ i, (∑ m, ⟪w m, v i⟫_ℝ • w m) ⊗ₜ[ℝ] ∑ n, ⟪w n, v i⟫_ℝ • w n := by
+    simp only [sum_tmul, tmul_sum, smul_tmul_smul, Finset.sum_comm (γ := ι), Finset.sum_smul]
+    rw [Finset.sum_comm]
+  _ = ∑ i, v i ⊗ₜ[ℝ] v i := by
+    simp only [w.sum_repr' (v _)]
+
+
+noncomputable def Real.Laplace (f : E → E) : E → E :=
+  fun x ↦ tensor_of_iteratedFDeriv_two ℝ f x (InnerProductSpace.canonicalCovariantTensor E)
+
+notation "Δ" => Real.Laplace
