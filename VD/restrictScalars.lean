c563b60752ac4a37709bc68abdbe9f53eb161adf
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-06-24 11:21:27 +0200

    Working…

--- VD/restrictScalars.lean
+++ VD/restrictScalars.lean
@@ -1,64 +1,59 @@
-import Mathlib.Analysis.Calculus.FDeriv.Congr
+import Mathlib.Analysis.Calculus.FDeriv.RestrictScalars
 import Mathlib.Analysis.Calculus.IteratedDeriv.Defs
-import VD.Harmonic
 
 variable
-  {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E] [NormedSpace ℂ E]
-  {F : Type*} [NormedAddCommGroup F] [NormedSpace ℂ F] [IsScalarTower ℝ ℂ F]
-  {G : Type*} [NormedAddCommGroup G] [NormedSpace ℝ G]
-  {f f₁ f₂ : ℂ → F}
-  {s t : Set E} {c : ℝ}
+  {𝕜 : Type*} [NontriviallyNormedField 𝕜]
+  {𝕜' : Type*} [NontriviallyNormedField 𝕜'] [NormedAlgebra 𝕜 𝕜']
+  {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E] [NormedSpace 𝕜' E] [IsScalarTower 𝕜 𝕜' E]
+  {F : Type*} [NormedAddCommGroup F] [NormedSpace 𝕜 F] [NormedSpace 𝕜' F] [IsScalarTower 𝕜 𝕜' F]
+  {x : E}
 
 open Topology
 
-variable {F : Type*} [NormedAddCommGroup F] [NormedSpace ℂ F]
-
-theorem fderiv_restrictScalarsLinear_comp {n : ℕ} {x : E}
-    {f : E → (ContinuousMultilinearMap ℂ (fun _ : Fin n ↦ E) F)}
-    (h : DifferentiableAt ℂ f x) :
-    (fderiv ℝ ((ContinuousMultilinearMap.restrictScalarsLinear ℝ) ∘ f) x)
-      = (ContinuousMultilinearMap.restrictScalars ℝ) ∘ ((fderiv ℂ f x).restrictScalars ℝ) := by
-  rw [fderiv_comp _ (by fun_prop) (h.restrictScalars ℝ), ContinuousLinearMap.fderiv]
+theorem fderiv_restrictScalarsLinear_comp {n : ℕ}
+    {f : E → (ContinuousMultilinearMap 𝕜' (fun _ : Fin n ↦ E) F)}
+    (h : DifferentiableAt 𝕜' f x) :
+    (fderiv 𝕜 ((ContinuousMultilinearMap.restrictScalarsLinear 𝕜) ∘ f) x)
+      = (ContinuousMultilinearMap.restrictScalars 𝕜) ∘ ((fderiv 𝕜' f x).restrictScalars 𝕜) := by
+  rw [fderiv_comp _ (by fun_prop) (h.restrictScalars 𝕜), ContinuousLinearMap.fderiv]
   ext a b
-  simp [h.fderiv_restrictScalars ℝ]
+  simp [h.fderiv_restrictScalars 𝕜]
 
-theorem ContDiffAt.differentiableAt_iteratedDeriv
-    {f : E → F} {x : E} {n : ℕ} {m : ℕ}
-    (h : ContDiffAt ℂ n f x) (hmn : m < n) :
-    DifferentiableAt ℂ (iteratedFDeriv ℂ m f) x := by
-  apply ContDiffAt.differentiableAt (n := 1)
-  apply h.iteratedFDeriv_right (i := m) (m := 1)
-  rw [Nat.lt_iff_add_one_le, add_comm] at hmn
-  rw [← WithTop.coe_one]
-  simp_all
-  sorry
-  sorry
+theorem ContDiffAt.differentiableAt_iteratedFDeriv
+    {f : E → F} {n : WithTop ℕ∞} {m : ℕ} {x : E} (h : ContDiffAt 𝕜 n f x) (hmn : ↑m < n) :
+    DifferentiableAt 𝕜 (iteratedFDeriv 𝕜 m f) x := by
+  rw [← differentiableWithinAt_univ]
+  convert (h.differentiableWithinAt_iteratedFDerivWithin hmn (by simp [uniqueDiffOn_univ]))
+  exact iteratedFDerivWithin_univ.symm
 
-theorem ContDiffAt.iteratedFDeriv_restrictScalars {f : E → F} {n : ℕ} {z : E}
-    (h : ContDiffAt ℂ n f z) :
-    (ContinuousMultilinearMap.restrictScalarsLinear ℝ) ∘ (iteratedFDeriv ℂ n f) =ᶠ[𝓝 z]
-      (iteratedFDeriv ℝ n f) := by
+theorem ContDiffAt.iteratedFDeriv_restrictScalars_eventuallyEq {f : E → F} {n : ℕ}
+    (h : ContDiffAt 𝕜' n f x) :
+    (ContinuousMultilinearMap.restrictScalarsLinear 𝕜) ∘ (iteratedFDeriv 𝕜' n f) =ᶠ[𝓝 x]
+      (iteratedFDeriv 𝕜 n f) := by
   induction n with
   | zero =>
     filter_upwards with a
     ext m
     simp [iteratedFDeriv_zero_apply m]
   | succ n hn =>
-    have : ContDiffAt ℂ n f z := by
-      apply h.of_le
-      apply Nat.cast_le.mpr
-      exact Nat.le_add_right n 1
+    have : ContDiffAt 𝕜' n f x := h.of_le (Nat.cast_le.mpr (n.le_add_right 1))
     have t₀ := hn this
     have t₁ := this.eventually
-    simp at t₁
+    simp only [ne_eq, ENat.natCast_ne_coe_top, not_false_eq_true, forall_const] at t₁
     filter_upwards [t₀.eventually_nhds, t₁.eventually_nhds,
       h.eventually (by simp)] with a h₁a h₂a h₃a
     rw [← Filter.EventuallyEq] at h₁a
     ext m
-    simp [iteratedFDeriv_succ_apply_left]
-    have := h₁a.fderiv_eq (𝕜 := ℝ)
-    rw [← this]
-    rw [fderiv_restrictScalarsLinear_comp]
-    simp
-    · apply h₃a.differentiableAt_iteratedDeriv (m := n)
+    simp only [ContinuousMultilinearMap.restrictScalarsLinear_apply, Function.comp_apply,
+      ContinuousMultilinearMap.coe_restrictScalars, iteratedFDeriv_succ_apply_left]
+    rw [← h₁a.fderiv_eq, fderiv_restrictScalarsLinear_comp]
+    · simp
+    · apply h₃a.differentiableAt_iteratedFDeriv
+      rw [Nat.cast_lt]
       simp
+
+theorem ContDiffAt.iteratedFDeriv_restrictScalars {f : E → F} {n : ℕ} {z : E}
+    (h : ContDiffAt 𝕜' n f z) :
+    ((ContinuousMultilinearMap.restrictScalarsLinear 𝕜) ∘ (iteratedFDeriv 𝕜' n f)) z =
+      iteratedFDeriv 𝕜 n f z :=
+  h.iteratedFDeriv_restrictScalars_eventuallyEq.eq_of_nhds
