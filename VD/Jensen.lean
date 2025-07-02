32d2e1b640597b28b3ca761587dcee497db2247b
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-07-02 08:23:25 +0200

    Working

--- VD/Jensen.lean
+++ VD/Jensen.lean
@@ -6,28 +6,12 @@ import Mathlib.MeasureTheory.Integral.CircleAverage
 import Mathlib.Analysis.SpecialFunctions.Integrability.LogMeromorphic
 import VD.specialFunctions_CircleIntegral_affine
 import VD.ToMathlib.TrailingCoefficientFactorizedRational
+import VD.ToMathlib.circleIntegrability
 
 open Filter MeromorphicAt MeromorphicOn Metric Real
 
 variable
   {E : Type*} [NormedAddCommGroup E] [NormedSpace ℂ E]
-
-/-!
-# Circle Integrability
--/
-
-theorem CircleIntegrable.const_smul {c : ℂ} {a R : ℝ} {f : ℂ → ℝ} (h : CircleIntegrable f c R) :
-    CircleIntegrable (a • f) c R := by
-  apply IntervalIntegrable.const_mul h
-
-theorem CircleIntegrable.const_smul_fun {c : ℂ} {a R : ℝ} {f : ℂ → ℝ} (h : CircleIntegrable f c R) :
-    CircleIntegrable (fun z ↦ a * f z) c R := by
-  apply CircleIntegrable.const_smul h
-
-theorem circleIntegrable_log_norm_factorizedRational {R : ℝ} {c : ℂ} (D : ℂ → ℤ) :
-    CircleIntegrable (∑ᶠ u, ((D u) * log ‖· - u‖)) c R :=
-  CircleIntegrable.finsum (fun _ ↦ (circleIntegrable_log_norm_meromorphicOn
-    (analyticOnNhd_id.sub analyticOnNhd_const).meromorphicOn).const_smul)
 
 /-!
 ## Circle Averages
