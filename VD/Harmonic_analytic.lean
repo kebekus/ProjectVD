f2dcb36ee7f9301760a077ea2acc69266a93999c
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-07-06 07:40:07 +0200

    Update Harmonic_analytic.lean

--- VD/Harmonic_analytic.lean
+++ VD/Harmonic_analytic.lean
@@ -15,6 +15,21 @@ variable
   {F : Type*} [NormedAddCommGroup F] [NormedSpace ℝ F]
   {f : ℂ → ℝ} {x : ℂ}
 
+theorem xx {l : ℂ →L[ℝ] ℂ} (h : l I = I • l 1) :
+    ∀ (a b : ℂ), l (a • b) = a • l b := by
+  intro a b
+  rw [(by simp : a = a.re + a.im • I)]
+  rw [(by simp : b = b.re + b.im • I)]
+  --repeat rw [smul_eq_mul]
+  rw [add_smul, add_smul]
+  rw [smul_add, smul_add]
+  repeat rw [l.map_add]
+  repeat rw [coe_smul]
+  repeat rw [l.map_smul]
+
+
+  sorry
+
 theorem CauchyRiemann₇  {G : Type*} [NormedAddCommGroup G] [NormedSpace ℂ G]
     {f : ℂ → G} :
     (DifferentiableAt ℂ f x) ↔ (DifferentiableAt ℝ f x) ∧
@@ -28,39 +43,25 @@ theorem CauchyRiemann₇  {G : Type*} [NormedAddCommGroup G] [NormedSpace ℂ G]
       map_add' := (fderiv ℝ f x).map_add'
       map_smul' m y := by
         rw [RingHom.id_apply]
-        rw [(by simp : m = m.re + m.im • I)]
-        rw [(by simp : y = y.re + y.im • I)]
-        rw [add_smul, smul_add, smul_add, add_smul,
-          ContinuousLinearMap.map_add, ContinuousLinearMap.map_add,
-          ContinuousLinearMap.map_add, ContinuousLinearMap.map_add,
-          smul_add, smul_add]
-        congr
-        · apply (fderiv ℝ f x).map_smul
-        · apply (fderiv ℝ f x).map_smul
-        · have : (m.im • I) • ↑y.re = (m.im • y.re) • I := by
-            repeat rw [← coe_smul]
-            repeat rw [smul_eq_mul]
-            rw [ofReal_mul, mul_assoc, mul_assoc]
-            congr 1
-            rw [mul_comm]
-          rw [this]
-          rw [ContinuousLinearMap.map_smul, h₂]
-          have : ofReal y.re = (y.re) • (1 : ℂ) := by simp
-          rw [this]
-          rw [ContinuousLinearMap.map_smul]
-          rw [← smul_assoc]
-          repeat rw [← coe_smul]
-          rw [← smul_assoc]
+        calc (fderiv ℝ f x) (m • y)
+        _ = (fderiv ℝ f x) ((m.re * y.re - m.im * y.im) + (m.re * y.im + m.im * y.re) • I) := by
           congr 1
-          · rw [smul_eq_mul]
-            rw [smul_eq_mul]
-            rw [smul_eq_mul]
-            rw [smul_eq_mul]
-            rw [ofReal_mul]
-            ring
-        ·
+          nth_rw 1 [(by simp : m = m.re + m.im • I)]
+          nth_rw 1 [(by simp : y = y.re + y.im • I)]
+          rw [add_smul, add_smul]
+          rw [smul_add, smul_add]
+          simp only [smul_eq_mul, real_smul, ofReal_mul]
+          ring_nf
+          simp
+          ring
+        _ = (fderiv ℝ f x) ((m.re * y.re - m.im * y.im) • (1 : ℂ) + (m.re * y.im + m.im * y.re) • I) := by
+          simp
+        _ = m • (fderiv ℝ f x) y := by
+          rw [(fderiv ℝ f x).map_add]
+          rw [(fderiv ℝ f x).map_smul]
+          rw [(fderiv ℝ f x).map_smul]
+          rw [h₂]
           sorry
-
     }
     rfl
 
