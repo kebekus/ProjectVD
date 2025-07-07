402266081d18cb8ea5fc36533b98863227bcec89
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-07-07 08:30:50 +0200

    Update Harmonic_analytic.lean

--- VD/Harmonic_analytic.lean
+++ VD/Harmonic_analytic.lean
@@ -4,10 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Stefan Kebekus
 -/
 import VD.ToMathlib.Harmonic
-import VD.ToMathlib.restrictScalars
 import Mathlib.Analysis.Complex.CauchyIntegral
 import Mathlib.Analysis.SpecialFunctions.Complex.Analytic
-
 
 open Complex InnerProductSpace Topology
 
@@ -15,23 +13,40 @@ variable
   {F : Type*} [NormedAddCommGroup F] [NormedSpace ℝ F]
   {f : ℂ → ℝ} {x : ℂ}
 
-theorem xx {l : ℂ →L[ℝ] ℂ} (h : l I = I • l 1) :
-    ∀ (a b : ℂ), l (a • b) = a • l b := by
+theorem xx {ℓ : ℂ →L[ℝ] ℂ} (h : ℓ I = I • ℓ 1) :
+    ∀ (a b : ℂ), ℓ (a • b) = a • ℓ b := by
   intro a b
   rw [(by simp : a = a.re + a.im • I)]
   rw [(by simp : b = b.re + b.im • I)]
   --repeat rw [smul_eq_mul]
   rw [add_smul, add_smul]
   rw [smul_add, smul_add]
-  repeat rw [l.map_add]
+  repeat rw [ℓ.map_add]
   repeat rw [coe_smul]
-  repeat rw [l.map_smul]
+  repeat rw [ℓ.map_smul]
+  have : (a.im • I) • ↑b.re = (a.im * b.re) • I := by
+    simp
+    ring
+  rw [this]
+  have : (a.im • I) • b.im • I = -(a.im * b.im) • (1 : ℂ) := by
+    have : ↑a.im * I * (↑b.im * I) = (↑a.im * ↑b.im) * (I * I) := by
+      ring
+    simp [this]
+  rw [this]
+  have : b.re = b.re • (1 : ℂ) := by
+    simp
+  rw [this]
+  repeat rw [ℓ.map_smul]
+  repeat rw [h]
+  repeat rw [← coe_smul]
+  repeat rw [smul_eq_mul]
+  simp
+  ring_nf
+  simp
+  abel_nf
 
-
-  sorry
-
-theorem CauchyRiemann₇  {G : Type*} [NormedAddCommGroup G] [NormedSpace ℂ G]
-    {f : ℂ → G} :
+theorem CauchyRiemann₇
+    {f : ℂ → ℂ} :
     (DifferentiableAt ℂ f x) ↔ (DifferentiableAt ℝ f x) ∧
       (fderiv ℝ f x I = I • fderiv ℝ f x 1) := by
   constructor
@@ -41,36 +56,38 @@ theorem CauchyRiemann₇  {G : Type*} [NormedAddCommGroup G] [NormedSpace ℂ G]
     use {
       toFun := fderiv ℝ f x
       map_add' := (fderiv ℝ f x).map_add'
-      map_smul' m y := by
-        rw [RingHom.id_apply]
-        calc (fderiv ℝ f x) (m • y)
-        _ = (fderiv ℝ f x) ((m.re * y.re - m.im * y.im) + (m.re * y.im + m.im * y.re) • I) := by
-          congr 1
-          nth_rw 1 [(by simp : m = m.re + m.im • I)]
-          nth_rw 1 [(by simp : y = y.re + y.im • I)]
-          rw [add_smul, add_smul]
-          rw [smul_add, smul_add]
-          simp only [smul_eq_mul, real_smul, ofReal_mul]
-          ring_nf
-          simp
-          ring
-        _ = (fderiv ℝ f x) ((m.re * y.re - m.im * y.im) • (1 : ℂ) + (m.re * y.im + m.im * y.re) • I) := by
-          simp
-        _ = m • (fderiv ℝ f x) y := by
-          rw [(fderiv ℝ f x).map_add]
-          rw [(fderiv ℝ f x).map_smul]
-          rw [(fderiv ℝ f x).map_smul]
-          rw [h₂]
-          sorry
+      map_smul' := xx h₂
     }
     rfl
 
 
 /--
-If `f : ℂ → ℝ` is harmonic at `x`, then `∂f/∂1 + I • ∂f/∂I` is complex
+If `f : ℂ → ℝ` is harmonic at `x`, then `∂f/∂1 - I • ∂f/∂I` is complex
 differentiable at `x`.
 -/
 theorem HarmonicAt.xx (hf : HarmonicAt f x) :
-    DifferentiableAt ℂ (ofRealCLM ∘ (fderiv ℝ f · 1) + I • ofRealCLM ∘ (fderiv ℝ f · I)) x := by
-
-  sorry
+    DifferentiableAt ℂ (ofRealCLM ∘ (fderiv ℝ f · 1) - I • ofRealCLM ∘ (fderiv ℝ f · I)) x := by
+  have := hf.1
+  apply CauchyRiemann₇.2
+  constructor
+  · fun_prop
+  · repeat rw [fderiv_add]
+    repeat rw [fderiv_sub]
+    repeat rw [fderiv_const_smul]
+    repeat rw [fderiv_comp]
+    simp
+    repeat rw [mul_add]
+    repeat rw [mul_sub]
+    ring_nf
+    simp
+    rw [add_comm]
+    rw [sub_eq_add_neg]
+    congr 1
+    · sorry
+    · have := hf.2
+      sorry
+    simp
+    fun_prop
+    repeat rw [fderiv_add]
+    sorry
+    sorry
