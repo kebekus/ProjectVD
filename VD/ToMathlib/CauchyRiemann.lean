8db2e3586a5ba9966b5ad3c22b9626fadf535081
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-07-07 09:14:25 +0200

    Working…

--- VD/ToMathlib/CauchyRiemann.lean
+++ VD/ToMathlib/CauchyRiemann.lean
@@ -3,16 +3,9 @@ Copyright (c) 2025 Stefan Kebekus. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Stefan Kebekus
 -/
-import VD.ToMathlib.Harmonic
-import VD.ToMathlib.CauchyRiemann
-import Mathlib.Analysis.Complex.CauchyIntegral
-import Mathlib.Analysis.SpecialFunctions.Complex.Analytic
+import Mathlib.Analysis.Complex.Conformal
 
-open Complex InnerProductSpace Topology
-
-variable
-  {F : Type*} [NormedAddCommGroup F] [NormedSpace ℝ F]
-  {f : ℂ → ℝ} {x : ℂ}
+open Complex
 
 /--
 Helper lemma for `differentiableAt_complex_iff_differentiableAt_real`: A real
@@ -22,7 +15,7 @@ to `I • ℓ 1`.
 theorem real_linearMap_map_smul_complex {ℓ : ℂ →ₗ[ℝ] ℂ} (h : ℓ I = I • ℓ 1) :
     ∀ (a b : ℂ), ℓ (a • b) = a • ℓ b := by
   intro a b
-  rw [(by simp : a = a.re + a.im • I), (by simp : b = b.re + b.im • I)]
+  rw [(by simp only [real_smul, re_add_im] : a = a.re + a.im • I), (by simp : b = b.re + b.im • I)]
   repeat rw [add_smul]
   repeat rw [smul_add]
   repeat rw [ℓ.map_add]
@@ -61,35 +54,3 @@ theorem differentiableAt_complex_iff_differentiableAt_real {f : ℂ → ℂ} :
       map_smul' := real_linearMap_map_smul_complex h₂
     }
     rfl
-
-
-/--
-If `f : ℂ → ℝ` is harmonic at `x`, then `∂f/∂1 - I • ∂f/∂I` is complex
-differentiable at `x`.
--/
-theorem HarmonicAt.xx (hf : HarmonicAt f x) :
-    DifferentiableAt ℂ (ofRealCLM ∘ (fderiv ℝ f · 1) - I • ofRealCLM ∘ (fderiv ℝ f · I)) x := by
-  have := hf.1
-  apply differentiableAt_complex_iff_differentiableAt_real.2
-  constructor
-  · fun_prop
-  · repeat rw [fderiv_add]
-    repeat rw [fderiv_sub]
-    repeat rw [fderiv_const_smul]
-    repeat rw [fderiv_comp]
-    simp
-    repeat rw [mul_add]
-    repeat rw [mul_sub]
-    ring_nf
-    simp
-    rw [add_comm]
-    rw [sub_eq_add_neg]
-    congr 1
-    · sorry
-    · have := hf.2
-      sorry
-    simp
-    fun_prop
-    repeat rw [fderiv_add]
-    sorry
-    sorry
