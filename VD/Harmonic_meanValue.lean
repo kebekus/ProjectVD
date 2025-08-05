651a10469acddcba362c3f0b504ede7c17913541
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-08-05 09:02:15 +0200

    Working

--- VD/Harmonic_meanValue.lean
+++ VD/Harmonic_meanValue.lean
@@ -8,40 +8,44 @@ import Mathlib.MeasureTheory.Integral.CircleAverage
 import VD.Harmonic_analytic2
 import VD.ToMathlib.Analytic_meanValue
 
+
 /-!
-# The Mean Value Property of Complex Differentiable Functions
+# The Mean Value Property of Harmonic Functions
 -/
 
 open InnerProductSpace Metric Real
 
 variable
-  {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
-  {F : Type*} [NormedAddCommGroup F] [NormedSpace ℝ F] [CompleteSpace F]
+  {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] --[CompleteSpace E]
   {f : ℂ → ℝ} {c : ℂ} {R : ℝ}
+
+/-- If two functions agree on the circle, then their circle averages agree. -/
+theorem circleAverage_congr_sphere {f₁ f₂ : ℂ → E} (hf : Set.EqOn f₁ f₂ (sphere c |R|)) :
+    circleAverage f₁ c R = circleAverage f₂ c R := by
+  unfold circleAverage
+  congr 1
+  apply intervalIntegral.integral_congr
+  intro x hx
+  simp [hf (circleMap_mem_sphere' c R x)]
 
 /--
 The **Mean Value Property** of harmonic functions: If `f : ℂ → E` is harmonic in
 a neighborhood of a closed disc of radius `R` and center `c`, then the circle
 average `circleAverage f c R` equals `f c`.
 -/
-theorem circleAverage_of_harmonic (hf : HarmonicOnNhd f (closedBall c |R|)) (hR : 0 < R) :
+theorem circleAverage_of_harmonic (hf : HarmonicOnNhd f (closedBall c |R|)) :
     circleAverage f c R = f c := by
-  obtain ⟨e, h₁e, h₂e⟩ := IsCompact.exists_thickening_subset_open (isCompact_closedBall c |R|)
+  obtain ⟨e, h₁e, h₂e⟩ := (isCompact_closedBall c |R|).exists_thickening_subset_open
     (isOpen_setOf_harmonicAt f) hf
   rw [thickening_closedBall h₁e (abs_nonneg R)] at h₂e
   obtain ⟨F, h₁F, h₂F⟩ := harmonic_is_realOfHolomorphic (add_pos_of_pos_of_nonneg h₁e (abs_nonneg R)) h₂e
   have h₃F : ∀ z ∈ closedBall c |R|, DifferentiableAt ℂ F z :=
     fun x hx ↦ (h₁F x (by simp_all [lt_add_of_pos_of_le h₁e hx])).differentiableAt
-  have := circleAverage_of_differentiable_on h₃F
-  have t₀ : f = Complex.reCLM ∘ F := by
-    sorry
-  rw [t₀]
-  rw [ContinuousLinearMap.circleAverage_comp_comm]
-  simp_all
-  refine ContinuousOn.circleIntegrable' ?_
-  refine continuousOn_of_forall_continuousAt ?_
-  intro x hx
-  have : x ∈ closedBall c |R| := by
-    sorry
-  have := h₃F x this
-  fun_prop
+  have h₄F : Set.EqOn (Complex.reCLM ∘ F) f (sphere c |R|) :=
+    fun x hx ↦ h₂F (sphere_subset_ball (lt_add_of_pos_left |R| h₁e) hx)
+  rw [← circleAverage_congr_sphere h₄F, Complex.reCLM.circleAverage_comp_comm,
+    circleAverage_of_differentiable_on h₃F]
+  · apply h₂F
+    simp [mem_ball, dist_self, add_pos_of_pos_of_nonneg h₁e (abs_nonneg R)]
+  · exact (continuousOn_of_forall_continuousAt
+      (fun x hx ↦ (h₃F x (sphere_subset_closedBall hx)).continuousAt)).circleIntegrable'
