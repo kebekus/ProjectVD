4cbd31c0e7d809ff0434cc2334d421f9e062f05b
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-08-12 16:13:53 +0200

    Update test.lean

--- VD/test.lean
+++ VD/test.lean
@@ -15,50 +15,31 @@ import Mathlib.MeasureTheory.Integral.CircleAverage
 
 open Complex InnerProductSpace Real Topology
 
-variable
-  {f : ℂ → ℝ} {r : ℝ}
+theorem xx {f : ℂ → ℂ} (hf : DifferentiableOn ℂ f (Metric.closedBall (0 : ℂ) 1)) :
+    0 = 1 := by
 
-/-!
-## Circle Averages as a Function of the Radius
--/
+  have CPS := cauchyPowerSeries f 0 1
+  have IPS : FormalMultilinearSeries ℂ ℂ ℂ := by
+    intro n
+    by_cases n = 0
+    · exact 0
+    have := CPS (n - 1) (fun _ ↦ 1)
+    apply ContinuousMultilinearMap.mkPiRing ℂ (Fin n) (this / n)
 
-lemma continuousAt_circleAverage (h : ContDiffOn ℝ 0 f (Metric.sphere 0 r)) :
-    ContinuousAt (circleAverage f 0) r := by
-  unfold circleAverage
-  have ε : ℝ := by
-    sorry
-  have h₁ε : 0 < ε := by
-    sorry
-  have a : Set ℂ := Metric.closedBall 0 (r + ε) \ Metric.ball 0 (r - ε)
-  have h₁a : IsCompact a := by
-    sorry
-  have h₂a : ∀ ρ t, a ∈ 𝓝 (circleMap 0 ρ t) := by
-    sorry
-  have n : Set ℝ := Metric.ball 0 (r + ε) \ Metric.closedBall 0 (r - ε)
-  have hn : n ∈ 𝓝 r := by
-    sorry
-  have h₂n : ∀ ρ : ℝ, dist ρ r < ε → ∀ t : ℝ, circleMap 0 ρ t ∈ a := by
-    sorry
-  have h₂f : ContinuousOn f a := by
-    sorry
-  apply ContinuousAt.mul (by fun_prop)
-  apply intervalIntegral.continuousAt_of_dominated_interval
-  · apply Metric.eventually_nhds_iff.mpr
-    use ε, h₁ε
-    intro ρ hρ
-    apply ContinuousOn.aestronglyMeasurable
-    · intro t ht
-      apply ContinuousAt.continuousWithinAt
-      apply ContinuousAt.comp' _ (by fun_prop)
-      exact (continuousWithinAt_iff_continuousAt (h₂a ρ t)).1 (h₂f (circleMap 0 ρ t) (h₂n ρ hρ t))
-    · exact measurableSet_uIoc
-  ·
-    sorry
-  · sorry
-  · sorry
-  · sorry
+  have := IPS.derivSeries
 
-lemma xx {r : ℝ} :
-    DifferentiableAt ℝ (circleAverage f 0) r := by
-  unfold circleAverage
+
+
+
+  have A := hf.hasFPowerSeriesOnBall one_pos
+
+  have := A.hasSum (y := 0.5)
+
+  have B := cauchyPowerSeries f 0 ↑1
+
+  have := B 2 (fun _ ↦ 0)
+
+
+
+  simp_all
   sorry
