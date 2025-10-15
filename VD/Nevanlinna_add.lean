0bff87a68688c9fbfa1555e92115c905f0cbcb8f
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-10-15 15:40:43 +0200

    Update

--- VD/Nevanlinna_add.lean
+++ VD/Nevanlinna_add.lean
@@ -1,4 +1,4 @@
-import VD.ToMathlib.Nevanlinna_mul
+import VD.Nevanlinna_add1
 
 open MeromorphicOn Metric Real Set Classical
 
@@ -7,6 +7,12 @@ variable
   {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
   {U : Set 𝕜} {f g : 𝕜 → E} {a : WithTop E} {a₀ : E}
 
+theorem meromorphicOrderAt_add_top
+    {f₁ f₂ : 𝕜 → E} {x : 𝕜} (hf₁ : meromorphicOrderAt f₁ x = ⊤) :
+    meromorphicOrderAt (f₁ + f₂) x = meromorphicOrderAt f₂ x := by
+  rw [meromorphicOrderAt_congr]
+  filter_upwards [meromorphicOrderAt_eq_top_iff.1 hf₁] with z hz
+  simp_all
 
 @[simp]
 theorem WithTop.max_untop₀ {α : Type*} [AddCommGroup α] [LinearOrder α] {a b : WithTop α}
@@ -59,10 +65,10 @@ variable
   {Y : Type*} [AddCommGroup Y] [LinearOrder Y]
 
 lemma posPart_apply (a : locallyFinsuppWithin U Y) (x : X) :
-    a⁺ x = (a x)⁺ := by rfl
+    a⁺ x = (a x)⁺ := rfl
 
 lemma negPart_apply (a : locallyFinsuppWithin U Y) (x : X) :
-    a⁻ x = (a x)⁻ := by rfl
+    a⁻ x = (a x)⁻ := rfl
 
 variable [IsOrderedAddMonoid Y]
 
@@ -77,60 +83,85 @@ theorem neg_min (a b : locallyFinsuppWithin U Y) :
 
 end Function.locallyFinsuppWithin
 
+theorem neg_min {Y : Type*} [AddCommGroup Y] [LinearOrder Y] [AddLeftMono Y] (a b : Y) :
+    (min a b)⁻ = max a⁻ b⁻ := by
+  rcases lt_trichotomy a b with h | h | h
+  · rw [min_eq_left h.le, max_comm, max_eq_right ((le_iff_posPart_negPart a b).1 h.le).2]
+  · simp_all
+  · rw [min_comm, min_eq_left h.le, max_eq_right ((le_iff_posPart_negPart b a).1 h.le).2]
+
 namespace ValueDistribution
 
 variable [ProperSpace 𝕜]
 
 
-theorem xx₁ {f₁ f₂ : ℂ → ℂ} {z : ℂ} {U : Set ℂ} (hf₁ : MeromorphicOn f₁ U)  (hf₂ : MeromorphicOn f₂ U)
-    (h₁z : z ∈ U) (h₃ : meromorphicOrderAt (f₁ + f₂) z ≠ ⊤) :
+theorem min_divisor_le_divisor_add [NormedSpace ℂ E] {f₁ f₂ : ℂ → E} {z : ℂ} {U : Set ℂ} (hf₁ : MeromorphicOn f₁ U)
+    (hf₂ : MeromorphicOn f₂ U) (h₁z : z ∈ U) (h₃ : meromorphicOrderAt (f₁ + f₂) z ≠ ⊤) :
     min (divisor f₁ U z) (divisor f₂ U z) ≤ divisor (f₁ + f₂) U z := by
   by_cases hz : z ∉ U
   · simp_all
   simp only [Decidable.not_not] at hz
   rw [divisor_apply hf₁ hz, divisor_apply hf₂ hz, divisor_apply (hf₁.add hf₂) hz]
   by_cases h₁ : meromorphicOrderAt f₁ z = ⊤
-  · sorry
-  have h₂ : meromorphicOrderAt f₂ z ≠ ⊤ := by sorry
+  · simp
+    right
+    rwa [meromorphicOrderAt_add_top]
+  by_cases h₂ : meromorphicOrderAt f₂ z = ⊤
+  · simp
+    left
+    rwa [add_comm, meromorphicOrderAt_add_top]
   rw [← WithTop.min_untop₀ h₁ h₂]
-  apply WithTop.untop₀_le_untop₀_of_le (h₃ z hz)
+  apply WithTop.untop₀_le_untop₀_of_le h₃
   exact meromorphicOrderAt_add (hf₁ z hz) (hf₂ z hz)
 
-
-theorem xx₂ {f₁ f₂ : ℂ → ℂ} {U : Set ℂ} (hf₁ : MeromorphicOn f₁ U)  (hf₂ : MeromorphicOn f₂ U) :
-    (divisor (f₁ + f₂) U)⁻ ≤ (divisor f₁ U)⁻ + (divisor f₂ U)⁻ := by
+theorem negPart_divisor_add_le_max [NormedSpace ℂ E] {f₁ f₂ : ℂ → E} {U : Set ℂ} (hf₁ : MeromorphicOn f₁ U)
+    (hf₂ : MeromorphicOn f₂ U) :
+    (divisor (f₁ + f₂) U)⁻ ≤ max (divisor f₁ U)⁻ (divisor f₂ U)⁻ := by
   intro z
   by_cases hz : z ∉ U
   · simp [hz]
   simp at hz
-  simp [Function.locallyFinsuppWithin.negPart_apply]
+  simp only [Function.locallyFinsuppWithin.negPart_apply, Function.locallyFinsuppWithin.max_apply]
   by_cases hf₁₂ : meromorphicOrderAt (f₁ + f₂) z = ⊤
-  · sorry
+  · simp [divisor_apply (hf₁.add hf₂) hz, hf₁₂, negPart_nonneg]
+  rw [← neg_min]
+  apply ((le_iff_posPart_negPart _ _).1 (min_divisor_le_divisor_add hf₁ hf₂ hz hf₁₂)).2
 
-  have := xx₁ hf₁ hf₂ hz hf₁₂
-
-  have A := ((le_iff_posPart_negPart (min (divisor f₁ U z) (divisor f₂ U z)) (divisor (f₁ + f₂) U z)).1 this).2
-  rw [Function.locallyFinsuppWithin.neg_min] at A
-  intro z
-
-  intro z
-  simp
-  rw [negPart]
-  --rw [instNegPart]
-  simp [instNegPart]
-  rw [Function.locallyFinsuppWithin.min_apply]
-
-  rw [Function.locallyFinsuppWithin.instNegPart]
-  sorry
+theorem negPart_divisor_add_le_add [NormedSpace ℂ E] {f₁ f₂ : ℂ → E} {U : Set ℂ} (hf₁ : MeromorphicOn f₁ U) (hf₂ : MeromorphicOn f₂ U) :
+    (divisor (f₁ + f₂) U)⁻ ≤ (divisor f₁ U)⁻ + (divisor f₂ U)⁻ := by
+  calc (divisor (f₁ + f₂) U)⁻
+  _ ≤ max (divisor f₁ U)⁻ (divisor f₂ U)⁻ :=
+    negPart_divisor_add_le_max hf₁ hf₂
+  _ ≤ (divisor f₁ U)⁻ + (divisor f₂ U)⁻ := by
+    by_cases h : (divisor f₁ U)⁻ ≤ (divisor f₂ U)⁻
+    · simp_all [negPart_nonneg]
+    simp_all [negPart_nonneg]
 
 /--
 The counting function of `f + g` at `⊤` is less than or equal to the sum of the
 counting functions of `f` and `g`.
 -/
-theorem counting_top_add_le {f₁ f₂ : ℂ → ℂ} (h₁f₁ : MeromorphicOn f₁ Set.univ)
-    (h₁f₂ : MeromorphicOn f₂ Set.univ) :
-    logCounting (f₁ + f₂) ⊤ ≤ (logCounting f₁ ⊤) + (logCounting f₂ ⊤) := by
-  simp [logCounting]
-  sorry
+theorem counting_top_add_le [NormedSpace ℂ E] {f₁ f₂ : ℂ → E} {r : ℝ}
+    (h₁f₁ : MeromorphicOn f₁ Set.univ) (h₁f₂ : MeromorphicOn f₂ Set.univ) (hr : 1 ≤ r) :
+    logCounting (f₁ + f₂) ⊤ r ≤ ((logCounting f₁ ⊤) + (logCounting f₂ ⊤)) r := by
+  simp only [logCounting, ↓reduceDIte]
+  rw [← Function.locallyFinsuppWithin.logCounting.map_add]
+  exact Function.locallyFinsuppWithin.logCounting_le (negPart_divisor_add_le_add h₁f₁ h₁f₂) hr
+
+theorem counting_top_sum_le [NormedSpace ℂ E] {α : Type*} (s : Finset α) (f : α → ℂ → E)
+    {r : ℝ} (h₁f : ∀ a, MeromorphicOn (f a) Set.univ) (hr : 1 ≤ r) :
+    logCounting (∑ a ∈ s, f a) ⊤ r ≤ (∑ a ∈ s, (logCounting (f a) ⊤)) r := by
+  induction s using Finset.induction with
+  | empty =>
+    simp_all
+    sorry
+  | insert a s ha hs =>
+    rw [Finset.sum_insert ha, Finset.sum_insert ha]
+    calc logCounting (f a + ∑ x ∈ s, f x) ⊤ r
+    _ ≤ (logCounting (f a) ⊤ + logCounting (∑ x ∈ s, f x) ⊤) r :=
+      counting_top_add_le (h₁f a) (MeromorphicOn.sum h₁f) hr
+    _ ≤ (logCounting (f a) ⊤ + ∑ x ∈ s, logCounting (f x) ⊤) r :=
+      add_le_add (by trivial) hs
+
 
 end ValueDistribution
