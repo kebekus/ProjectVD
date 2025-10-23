697c046addca9f009f88176e9b9492e8d0be9424
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-10-23 07:46:13 +0200

    Update Cartan.lean

--- VD/Cartan.lean
+++ VD/Cartan.lean
@@ -1,6 +1,21 @@
 import Mathlib.Analysis.Complex.ValueDistribution.FirstMainTheorem
+import VD.MathlibSubmitted.DivisorOrder
 
 open Function MeromorphicOn Metric Real Set Classical ValueDistribution
+
+namespace locallyFinsuppWithin
+
+variable
+  {X : Type*} [TopologicalSpace X] {U : Set X}
+  {Y : Type*}
+
+lemma restrictPosPart {V : Set X} (D : locallyFinsuppWithin U ℤ) (h : V ⊆ U) :
+    D⁺.restrict h = (D.restrict h)⁺ := by
+  ext x
+  simp [locallyFinsuppWithin.restrict_apply, locallyFinsuppWithin.posPart_apply]
+  aesop
+
+end locallyFinsuppWithin
 
 
 lemma characteristic_sub_characteristic_inv {a : ℂ} {f : ℂ → ℂ} (h : MeromorphicOn f ⊤) :
@@ -17,6 +32,16 @@ lemma characteristic_sub_characteristic_inv {a : ℂ} {f : ℂ → ℂ} (h : Mer
   unfold logCounting
   unfold locallyFinsuppWithin.logCounting
   simp
+
+  have {r : ℝ} : (locallyFinsuppWithin.toClosedBall r) (divisor (fun z ↦ f z - a) univ)⁺
+      = (divisor (fun z ↦ f z - a) (closedBall 0 |r|))⁺ := by
+    unfold locallyFinsuppWithin.toClosedBall
+    simp
+    rw [locallyFinsuppWithin.restrictPosPart]
+    rw [MeromorphicOn.divisor_restrict]
+    apply h.sub
+    exact MeromorphicOn.const a
+  simp_rw [this]
 
 
   sorry
