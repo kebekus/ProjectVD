fc5236ea7dcda22eea9d38763a6d295590c4ae5b
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-10-25 17:50:08 +0200

    Update Cartan.lean

--- VD/Cartan.lean
+++ VD/Cartan.lean
@@ -9,17 +9,28 @@ variable
   {X : Type*} [TopologicalSpace X] {U : Set X}
   {Y : Type*}
 
-lemma restrictPosPart {V : Set X} (D : locallyFinsuppWithin U ℤ) (h : V ⊆ U) :
+/--
+Restriction commutes with taking positive parts.
+-/
+lemma restrict_posPart {V : Set X} (D : locallyFinsuppWithin U ℤ) (h : V ⊆ U) :
     D⁺.restrict h = (D.restrict h)⁺ := by
   ext x
   simp only [locallyFinsuppWithin.restrict_apply, locallyFinsuppWithin.posPart_apply]
   aesop
 
-lemma restrictNegPart {V : Set X} (D : locallyFinsuppWithin U ℤ) (h : V ⊆ U) :
+/--
+Restriction commutes with taking negative parts.
+-/
+lemma restrict_negPart {V : Set X} (D : locallyFinsuppWithin U ℤ) (h : V ⊆ U) :
     D⁻.restrict h = (D.restrict h)⁻ := by
   ext x
   simp only [locallyFinsuppWithin.restrict_apply, locallyFinsuppWithin.negPart_apply]
   aesop
+
+@[simp]
+lemma toClosedBall_divisor {r : ℝ} {f : ℂ → ℂ} (h : MeromorphicOn f ⊤) :
+    (divisor f (closedBall 0 |r|)) = (locallyFinsuppWithin.toClosedBall r) (divisor f univ) := by
+  simp_all [locallyFinsuppWithin.toClosedBall]
 
 end locallyFinsuppWithin
 
@@ -27,13 +38,12 @@ lemma lcd {f : ℂ → ℂ} :
     locallyFinsuppWithin.logCounting (divisor f ⊤) = logCounting f 0 - logCounting f ⊤ := by
   simp [logCounting, ← locallyFinsuppWithin.logCounting.map_sub]
 
-
 /--
-Reformulation of Jensen's formula: Up to a constant, the logarithmic counting
-function attached to the divisor of a meromorphic function `f` equals the circle
-average of `log ‖f‖`.
+Over the complex numbers, present the logarithmic counting function attached to
+the divisor of a meromorphic function `f` as a circle average over `log ‖f ·‖`.
 
-See `MeromorphicOn.circleAverage_log_norm` for Jensen's formula in the original
+This is a reformulation of Jensen's formula of Complex Analysis. See
+`MeromorphicOn.circleAverage_log_norm` for Jensen's formula in the original
 context.
 -/
 theorem locallyFinsuppWithin.logCounting_eq_circleAverage_sub_const {R : ℝ} {f : ℂ → ℂ}
@@ -44,9 +54,6 @@ theorem locallyFinsuppWithin.logCounting_eq_circleAverage_sub_const {R : ℝ} {f
   simp only [MeromorphicOn.circleAverage_log_norm hR h₁f, locallyFinsuppWithin.logCounting, top_eq_univ, AddMonoidHom.coe_mk, ZeroHom.coe_mk,
     zero_sub, norm_neg, add_sub_cancel_right]
   congr 1
-  · have {r : ℝ} : (locallyFinsuppWithin.toClosedBall r) (divisor f univ)
-        = (divisor f (closedBall 0 |r|)) := by
-      unfold locallyFinsuppWithin.toClosedBall
-      aesop
-    simp_rw [this]
-  · aesop
+  · simp_all
+  · rw [divisor_apply, divisor_apply]
+    all_goals aesop
