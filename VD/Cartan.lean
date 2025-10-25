fdb79873c2177b0d1aa4cc72f544205797f87d7e
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-10-25 17:24:11 +0200

    Update Cartan.lean

--- VD/Cartan.lean
+++ VD/Cartan.lean
@@ -15,57 +15,38 @@ lemma restrictPosPart {V : Set X} (D : locallyFinsuppWithin U ℤ) (h : V ⊆ U)
   simp only [locallyFinsuppWithin.restrict_apply, locallyFinsuppWithin.posPart_apply]
   aesop
 
+lemma restrictNegPart {V : Set X} (D : locallyFinsuppWithin U ℤ) (h : V ⊆ U) :
+    D⁻.restrict h = (D.restrict h)⁻ := by
+  ext x
+  simp only [locallyFinsuppWithin.restrict_apply, locallyFinsuppWithin.negPart_apply]
+  aesop
+
 end locallyFinsuppWithin
 
+lemma lcd {f : ℂ → ℂ} :
+    locallyFinsuppWithin.logCounting (divisor f ⊤) = logCounting f 0 - logCounting f ⊤ := by
+  simp [logCounting, ← locallyFinsuppWithin.logCounting.map_sub]
 
-lemma characteristic_sub_characteristic_inv {a : ℂ} {f : ℂ → ℂ} (h : MeromorphicOn f ⊤) :
-    0  = logCounting f a := by
 
-  have R : ℝ := by sorry
-  have hR : R ≠ 0 := by sorry
+/--
+Reformulation of Jensen's formula: Up to a constant, the logarithmic counting
+function attached to the divisor of a meromorphic function `f` equals the circle
+average of `log ‖f‖`.
+
+See `MeromorphicOn.circleAverage_log_norm` for Jensen's formula in the original
+context.
+-/
+theorem locallyFinsuppWithin.logCounting_eq_circleAverage_sub_const {R : ℝ} {f : ℂ → ℂ}
+  (h : MeromorphicOn f ⊤) (hR : R ≠ 0) :
+    locallyFinsuppWithin.logCounting (divisor f ⊤) R =
+      circleAverage (log ‖f ·‖) 0 R - log ‖meromorphicTrailingCoeffAt f 0‖ := by
   have h₁f : MeromorphicOn f (closedBall 0 |R|) := by tauto
-
-  have jensen := MeromorphicOn.circleAverage_log_norm hR h₁f
-  simp at jensen
-
-  unfold logCounting
-  unfold locallyFinsuppWithin.logCounting
-  simp
-
-  have {r : ℝ} : (locallyFinsuppWithin.toClosedBall r) (divisor (fun z ↦ f z - a) univ)⁺
-      = (divisor (fun z ↦ f z - a) (closedBall 0 |r|))⁺ := by
-    unfold locallyFinsuppWithin.toClosedBall
-    simp
-    rw [locallyFinsuppWithin.restrictPosPart]
-    rw [MeromorphicOn.divisor_restrict]
-    apply h.sub
-    exact MeromorphicOn.const a
-  simp_rw [this]
-  clear this
-
-  have : (divisor f (closedBall 0 |R|))
-      = (divisor f (closedBall 0 |R|))⁺ - (divisor f (closedBall 0 |R|))⁻ := by
-    rw [posPart_sub_negPart]
-  rw [this] at jensen
-  clear this
-
-  have {u : ℂ} : (((divisor f (closedBall 0 |R|))⁺ - (divisor f (closedBall 0 |R|))⁻) u)
-      = ((divisor f (closedBall 0 |R|))⁺ u - (divisor f (closedBall 0 |R|))⁻ u) := by
-    rfl
-  simp_rw [this] at jensen
-  clear this
-
-  have {a b : ℤ} {c : ℝ} : (↑(a - b) : ℝ) * c = a * c - b * c := by
-    rw [Int.cast_sub a b]
-    exact sub_mul (↑a) (↑b) c
-  conv at jensen =>
-    right
-    arg 1
-    arg 1
-    arg 1
-    intro u
-    rw [this]
-  clear this
-
-  rw [finsum_sub_distrib] at jensen
-  sorry
+  simp only [MeromorphicOn.circleAverage_log_norm hR h₁f, locallyFinsuppWithin.logCounting, top_eq_univ, AddMonoidHom.coe_mk, ZeroHom.coe_mk,
+    zero_sub, norm_neg, add_sub_cancel_right]
+  congr 1
+  · have {r : ℝ} : (locallyFinsuppWithin.toClosedBall r) (divisor f univ)
+        = (divisor f (closedBall 0 |r|)) := by
+      unfold locallyFinsuppWithin.toClosedBall
+      aesop
+    simp_rw [this]
+  · aesop
