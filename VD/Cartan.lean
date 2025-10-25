1fc966cc3f7821528a407b2317fad0756573cfa9
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-10-25 07:50:52 +0200

    Update Cartan.lean

--- VD/Cartan.lean
+++ VD/Cartan.lean
@@ -42,6 +42,37 @@ lemma characteristic_sub_characteristic_inv {a : ℂ} {f : ℂ → ℂ} (h : Mer
     apply h.sub
     exact MeromorphicOn.const a
   simp_rw [this]
+  clear this
 
+  have : (divisor f (closedBall 0 |R|))
+      = (divisor f (closedBall 0 |R|))⁺ - (divisor f (closedBall 0 |R|))⁻ := by
+    rw [posPart_sub_negPart]
+  rw [this] at jensen
+  clear this
+
+  have {u : ℂ} : (((divisor f (closedBall 0 |R|))⁺ - (divisor f (closedBall 0 |R|))⁻) u)
+      = ((divisor f (closedBall 0 |R|))⁺ u - (divisor f (closedBall 0 |R|))⁻ u) := by
+    rfl
+  simp_rw [this] at jensen
+  clear this
+
+  have {a b : ℤ} {c : ℝ} : (↑(a - b) : ℝ) * c = a * c - b * c := by
+    rw [Int.cast_sub a b]
+    exact sub_mul (↑a) (↑b) c
+  conv at jensen =>
+    right
+    arg 1
+    arg 1
+    arg 1
+    intro u
+    rw [this]
+  clear this
+
+
+
+
+
+
+  simp_rw [this] at jensen
 
   sorry
