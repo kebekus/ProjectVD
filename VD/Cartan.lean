578b53d83b5f02b37df29c712a7d22e5ad8ba1f1
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-10-27 12:34:18 +0100

    Update Cartan.lean

--- VD/Cartan.lean
+++ VD/Cartan.lean
@@ -7,6 +7,14 @@ namespace ValueDistribution
 theorem cartan {r : ℝ} {f : ℂ → ℂ} (h : MeromorphicOn f ⊤) :
     characteristic f ⊤ r
       = circleAverage (logCounting f · r) 0 1 - log ‖meromorphicTrailingCoeffAt f 0‖ := by
+  have a : ℂ := by sorry
+  have h₁ : MeromorphicOn (fun z ↦ f z - a) ⊤ :=
+    h.sub (MeromorphicOn.const a)
+
+  have R : ℝ := by sorry
+  have hR : R ≠ 0 := by sorry
+  have := logCounting_zero_sub_logCounting_top_eq_circleAverage_sub_const h₁ hR
+
   sorry
 
 end ValueDistribution
