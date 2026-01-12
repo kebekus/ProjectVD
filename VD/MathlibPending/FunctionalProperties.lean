c1aea6ded96bfae8d2e5a5dc571fc3e7a94906a7
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-01-12 16:58:30 +0100

    Working…

---
+++ VD/MathlibPending/FunctionalProperties.lean
@@ -0,0 +1,161 @@
+import Mathlib
+
+open Function Metric Real Set Classical Topology ValueDistribution
+
+/-
+Establish positivity, parity and monotonocity of the Nevanlinna functions.
+-/
+
+theorem circleAverage.integral_undef {c : ℂ} {R : ℝ} {f : ℂ → ℝ} (hf : ¬CircleIntegrable f c R) :
+    circleAverage f c R = 0 := by
+  simp_all [circleAverage, CircleIntegrable, intervalIntegral.integral_undef]
+
+theorem circleAverage_nonneg_of_nonneg {c : ℂ} {R : ℝ} {f : ℂ → ℝ}
+    (h₂f : ∀ x ∈ Metric.sphere c |R|, 0 ≤ f x) :
+    0 ≤ circleAverage f c R := by
+  by_cases hf : CircleIntegrable f c R
+  · rw [← circleAverage_const 0 c |R|, circleAverage, circleAverage, smul_eq_mul, smul_eq_mul,
+      mul_le_mul_iff_of_pos_left (inv_pos.2 two_pi_pos)]
+    apply intervalIntegral.integral_mono_on_of_le_Ioo (le_of_lt two_pi_pos)
+      intervalIntegrable_const hf (fun θ _ ↦ h₂f (circleMap c R θ) (circleMap_mem_sphere' c R θ))
+  · rw [circleAverage.integral_undef hf]
+
+namespace Function.locallyFinsuppWithin
+
+variable
+  {E : Type*} [NormedAddCommGroup E] [ProperSpace E]
+
+/--
+The logarithmic counting function is even.
+-/
+lemma logCounting_even (D : locallyFinsuppWithin (univ : Set E) ℤ) :
+    (logCounting D).Even := fun r ↦ by simp [logCounting, toClosedBall, restrict_apply]
+
+lemma toClosedBall_support_subset_closedBall {E : Type*} [NormedAddCommGroup E] {r : ℝ}
+    (f : locallyFinsuppWithin (univ : Set E) ℤ) :
+    (toClosedBall r f).support ⊆ closedBall 0 |r| := by
+  simp_all [toClosedBall, restrict_apply]
+
+/--
+The logarithmic counting function is monotonous.
+-/
+lemma logCounting_mono {D : locallyFinsuppWithin (univ : Set E) ℤ} (hD : 0 ≤ D) :
+    MonotoneOn (logCounting D) (Ioi 0) := by
+  intro a ha b hb _
+  simp_all only [mem_Ioi, logCounting, AddMonoidHom.coe_mk, ZeroHom.coe_mk]
+  gcongr
+  · let s := (toClosedBall b D).support
+    have hs : s.Finite := (toClosedBall b D).finiteSupport (isCompact_closedBall 0 |b|)
+    repeat rw [finsum_eq_sum_of_support_subset (s := hs.toFinset)]
+    · gcongr 1 with z hz
+      by_cases h₂z : z = 0
+      · simp [h₂z]
+      · have := (toClosedBall_support_subset_closedBall D (hs.mem_toFinset.1 hz))
+        rw [toClosedBall_eval_within _ this]
+        by_cases h₃z : z ∈ closedBall 0 |a|
+        · rw [toClosedBall_eval_within _ h₃z]
+          gcongr
+          exact Int.cast_nonneg (hD z)
+        · simp only [h₃z, not_false_eq_true, apply_eq_zero_of_notMem, Int.cast_zero, zero_mul,
+            ge_iff_le]
+          apply mul_nonneg (Int.cast_nonneg (hD z)) (log_nonneg _)
+          apply (le_mul_inv_iff₀ (norm_pos_iff.mpr h₂z)).2
+          simp_all [abs_of_pos hb]
+    · intro z
+      aesop
+    · intro z
+      simp only [support_mul, mem_inter_iff, mem_support, ne_eq, Int.cast_eq_zero, log_eq_zero,
+        mul_eq_zero, inv_eq_zero, norm_eq_zero, not_or, Finite.coe_toFinset, and_imp, s]
+      intro h₁ _ _ _ _
+      have : z ∈ closedBall 0 |a| := mem_of_indicator_ne_zero h₁
+      rw [toClosedBall_eval_within _ this] at h₁
+      rwa [toClosedBall_eval_within]
+      · simp_all only [abs_of_pos ha, mem_closedBall, dist_zero_right, abs_of_pos hb]
+        linarith
+  · exact Int.cast_nonneg (hD 0)
+
+end Function.locallyFinsuppWithin
+
+namespace ValueDistribution
+
+/--
+The proximity function is non-negative.
+-/
+theorem proximity_nonneg {f : ℂ → ℂ} {a : WithTop ℂ} :
+    0 ≤ proximity f a := by
+  by_cases h : a = ⊤
+  all_goals
+    intro r
+    simp only [Pi.zero_apply, proximity, h, ↓reduceDIte]
+    exact circleAverage_nonneg_of_nonneg (fun x _ ↦ posLog_nonneg)
+
+/--
+The logarithmic counting function is even.
+-/
+theorem logCounting_even {f : ℂ → ℂ} {a : WithTop ℂ} :
+    Function.Even (logCounting f a) := by
+  intro r
+  by_cases h : a = ⊤
+  all_goals simp [logCounting, h, Function.locallyFinsuppWithin.logCounting_even _ r]
+
+/--
+The proximity function is even.
+-/
+theorem proximity_even {f : ℂ → ℂ} {a : WithTop ℂ} :
+    Function.Even (proximity f a) := by
+  intro r
+  by_cases h : a = ⊤
+  all_goals simp [proximity, h]
+
+/--
+The characteristic function is even.
+-/
+theorem characteristic_even {f : ℂ → ℂ} {a : WithTop ℂ} :
+    Function.Even (characteristic f a) := Function.Even.add proximity_even logCounting_even
+
+/--
+The logarithmic counting function is monotonous.
+-/
+theorem logCounting_monotoneOn {f : ℂ → ℂ} {a : WithTop ℂ} :
+    MonotoneOn (logCounting f a) (Ioi 0) := by
+  unfold logCounting
+  by_cases h : a = ⊤
+  · simp only [h]
+    apply locallyFinsuppWithin.logCounting_mono (negPart_nonneg _)
+  · simp only [h]
+    apply locallyFinsuppWithin.logCounting_mono (posPart_nonneg _)
+
+/--
+For `1 ≤ r`, the logarithmic counting function is non-negative.
+-/
+theorem logCounting_nonneg {r : ℝ} {f : ℂ → ℂ} {a : WithTop ℂ} (hr : 1 ≤ r) :
+    0 ≤ logCounting f a r := by
+  by_cases h : a = ⊤
+  · simp [logCounting, h, locallyFinsuppWithin.logCounting_nonneg
+      (negPart_nonneg (MeromorphicOn.divisor f univ)) hr]
+  · simp [logCounting, h, locallyFinsuppWithin.logCounting_nonneg
+      (posPart_nonneg (MeromorphicOn.divisor (f · - a.untop₀) univ)) hr]
+
+/--
+The proximity function is asymptotically non-negative.
+-/
+theorem logCounting_eventually_nonneg {f : ℂ → ℂ} {a : WithTop ℂ} :
+    0 ≤ᶠ[Filter.atTop] logCounting f a  := by
+  filter_upwards [Filter.eventually_ge_atTop 1] using fun _ hr ↦ by simp [logCounting_nonneg hr]
+
+/--
+For `1 ≤ r`, the characteristic function is non-negative.
+-/
+theorem characteristic_nonneg {r : ℝ} {f : ℂ → ℂ} {a : WithTop ℂ} (hr : 1 ≤ r) :
+    0 ≤ characteristic f a r := by
+  simp only [characteristic, Pi.add_apply]
+  exact add_nonneg (proximity_nonneg r) (logCounting_nonneg hr)
+
+/--
+The proximity function is asymptotically non-negative.
+-/
+theorem characteristic_eventually_nonneg {f : ℂ → ℂ} {a : WithTop ℂ} :
+    0 ≤ᶠ[Filter.atTop] characteristic f a  := by
+  filter_upwards [Filter.eventually_ge_atTop 1] using fun _ hr ↦ by simp [characteristic_nonneg hr]
+
+end ValueDistribution
