d207100807720daad94f3c755f9d659a9d628521
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-01-13 07:42:54 +0100

    Update VD.lean

--- VD.lean
+++ VD.lean
@@ -1,5 +1,6 @@
+import VD.Boundedness
 import VD.Cartan
 import VD.CartanExperimentation
 import VD.FunctionalProperties
-import VD.MathlibSubmitted.Nevanlinna_add_characteristic
+import VD.MathlibSubmitted.FunctionalProperties
 import VD.leftovers.meromorphicAt_topology
