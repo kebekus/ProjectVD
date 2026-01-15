8ffd2311d0e9ca24d7f45505e8083e0a4d110c87
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-01-15 16:57:29 +0100

    Update BoundednessLogCounting.lean

--- VD/BoundednessLogCounting.lean
+++ VD/BoundednessLogCounting.lean
@@ -13,8 +13,60 @@ symptotically like `log`.
 
 namespace Function.locallyFinsuppWithin
 
+lemma elementIndicatorSupport {X : Type*} {e : X} :
+    (if · = e then 1 else 0 : X → ℤ).support = {e} := by
+  aesop
+
 variable
-  {E : Type*} [NormedAddCommGroup E] [ProperSpace E]
+  {E : Type*} [NormedAddCommGroup E] --[ProperSpace E]
+
+noncomputable def singleton (e : E) :
+    Function.locallyFinsuppWithin (Set.univ : Set E) ℤ where
+  toFun := fun z ↦ if z = e then 1 else 0
+  supportWithinDomain' z hz := by tauto
+  supportLocallyFiniteWithinDomain' := by
+    intro _ _
+    use ⊤
+    simp [(by aesop : (if · = e then 1 else 0 : E → ℤ).support = {e})]
+
+lemma xx {e₁ e₂ : E} :
+    singleton e₁ e₂ = if e₂ = e₁ then 1 else 0 := by
+  have : singleton e₁ e₂ = (singleton e₁).toFun e₂ := rfl
+  rw [this]
+  unfold singleton
+  simp
+
+lemma xxy [ProperSpace E] {e : E} :
+    logCounting (singleton e) =O[atTop] log := by
+  simp [logCounting]
+  rw [isBigO_iff]
+  use 2
+  rw [eventually_atTop]
+  use ‖e‖
+  intro b hb
+
+  rw [finsum_eq_sum_of_support_subset _ (s := (finite_singleton e).toFinset)]
+  simp
+  rw [toClosedBall_eval_within]
+  rw [xx]
+  simp
+  by_cases h : e = 0
+  · simp [h, xx]
+    sorry
+  · simp [xx]
+    rw [eq_comm] at h
+    simp [h]
+    rw [log_mul]
+
+    sorry
+
+  simp [singleton]
+
+  rw [FunLike.coe_injective]
+  rw []
+
+
+  sorry
 
 lemma zero_iff_logCounting_bounded {D : locallyFinsuppWithin (univ : Set E) ℤ} (h : 0 ≤ D) :
     D = 0 ↔ logCounting D =O[atTop] (1 : ℝ → ℝ) := by
