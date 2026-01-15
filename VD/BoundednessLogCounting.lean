00874f2ab14540d1afc47add438f6830bdeb6488
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-01-15 11:03:47 +0100

    Update BoundednessLogCounting.lean

--- VD/BoundednessLogCounting.lean
+++ VD/BoundednessLogCounting.lean
@@ -1,7 +1,7 @@
 import VD.Cartan
 import Mathlib
 
-open Asymptotics Function Metric Real Set Classical Topology ValueDistribution
+open Asymptotics Filter Function Metric Real Set Classical Topology ValueDistribution
 
 /-
 Prove that the logarithmic counting function of a meromorphic function `f` is
@@ -17,10 +17,21 @@ variable
   {E : Type*} [NormedAddCommGroup E] [ProperSpace E]
 
 lemma zero_iff_logCounting_bounded {D : locallyFinsuppWithin (univ : Set E) ℤ} (h : 0 ≤ D) :
-    D = 0 ↔ logCounting D =O[Filter.atTop] (1 : ℝ → ℝ) := by
+    D = 0 ↔ logCounting D =O[atTop] (1 : ℝ → ℝ) := by
   constructor
   · intro h₂
     simp [isBigO_of_le' (c := 0), h₂]
-  · sorry
+  · contrapose
+    intro h₁
+    obtain ⟨z, hz⟩ := (by simpa [locallyFinsuppWithin.ext_iff] using h₁ : ∃ z, D z ≠ 0)
+    rw [isBigO_iff]
+    push_neg
+    intro a
+    simp
+    rw [frequently_atTop]
+    intro b
+    simp [logCounting]
+
+    sorry
 
 end Function.locallyFinsuppWithin
