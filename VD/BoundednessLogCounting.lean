3883a5e760d6d730f41ff92da68b87abaec01519
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-01-20 13:46:21 +0100

    Update BoundednessLogCounting.lean

--- VD/BoundednessLogCounting.lean
+++ VD/BoundednessLogCounting.lean
@@ -3,7 +3,6 @@ import Mathlib
 
 open Asymptotics Filter Function Metric Real Set Classical Topology ValueDistribution
 
-
 /-
 Prove that the logarithmic counting function of a meromorphic function `f` is
 bounded if and only if `f` is constant.
@@ -29,18 +28,16 @@ noncomputable def single (e : E) :
 lemma single_eval {e₁ e₂ : E} :
     single e₁ e₂ = if e₂ = e₁ then 1 else 0 := rfl
 
-lemma single_nonneg {e : E} :
-    0 ≤ single e := by
-  intro x
-  by_cases he : x = e
-  all_goals
-    simp_all [single_eval]
-
 lemma single_pos {e : E} :
     0 < single e := by
-  apply lt_of_le_of_ne single_nonneg (DFunLike.ne_iff.2 _)
-  use e
-  simp [single_eval]
+  apply lt_of_le_of_ne
+  · intro x
+    by_cases he : x = e
+    all_goals
+      simp_all [single_eval]
+  · apply DFunLike.ne_iff.2
+    use e
+    simp [single_eval]
 
 lemma logCounting_single [ProperSpace E] {e : E} {r : ℝ} (hr : ‖e‖ ≤ r) :
     logCounting (single e) r = log r - log ‖e‖ := by
@@ -56,111 +53,59 @@ lemma logCounting_single [ProperSpace E] {e : E} {r : ℝ} (hr : ‖e‖ ≤ r) 
       (inv_ne_zero (norm_ne_zero_iff.mpr (he ·.symm))), log_inv]
     ring
 
-lemma xx [ProperSpace E] {D : locallyFinsuppWithin (univ : Set E) ℤ}
-    (h₁ : 0 ≤ D) (h₂ : D ≠ 0) :
+lemma le_single [ProperSpace E] {D : locallyFinsuppWithin (univ : Set E) ℤ} (h : 0 < D) :
     ∃ e, single e ≤ D := by
-  obtain ⟨z, hz⟩ := (by simpa [locallyFinsuppWithin.ext_iff] using h₂ : ∃ z, D z ≠ 0)
+  obtain ⟨z, hz⟩ := (by simpa [D.ext_iff] using (ne_of_lt h).symm : ∃ z, D z ≠ 0)
   use z
   intro e
-  simp [single_eval]
   by_cases he : e = z
-  · simp [he]
-    have : 1 ≤ D e := by
-      have := h₁ e
-      simp_all
-      have : 0 < D e := by
-        refine Int.lt_iff_le_and_ne.mpr ?_
-        simp_all
-        tauto
-      aesop
-    aesop
-  aesop
+  · subst he
+    simpa [single_eval] using Int.lt_iff_le_and_ne.mpr ⟨h.le e, hz.symm⟩
+  · simpa [he, single_eval] using h.le e
 
 lemma logCounting_strictMono [ProperSpace E] {D : locallyFinsuppWithin (univ : Set E) ℤ} {e : E}
     (hD : single e ≤ D) :
     StrictMonoOn (logCounting D) (Ioi ‖e‖) := by
-  have : D = (single e) + (D - single e) := by
-    abel
-  have : logCounting D = logCounting (single e) + logCounting (D - single e) := by
-    rw [this]
-    have := logCounting.map_add (single e) (D - single e)
-    rw [this]
-    simp
-  rw [this]
-  have : logCounting (single e) + logCounting (D - single e) =
-    fun r ↦ logCounting (single e) r + logCounting (D - single e) r := rfl
-  rw [this]
+  rw [(by aesop : logCounting D = logCounting (single e) + logCounting (D - single e))]
   apply StrictMonoOn.add_monotone
   · intro a ha b hb hab
-    simp at ha hb
-    rw [logCounting_single (e := e) ha.le]
-    rw [logCounting_single (e := e) hb.le]
+    rw [mem_Ioi] at ha hb
+    rw [logCounting_single (e := e) ha.le, logCounting_single (e := e) hb.le]
     gcongr
-    apply (norm_nonneg e).trans_lt ha
-  · intro a ha b hb hab
-    apply logCounting_mono
-    simp [hD]
-    simp at ⊢ ha hb
     exact (norm_nonneg e).trans_lt ha
-    exact (norm_nonneg e).trans_lt hb
-    exact hab
-
-lemma logCounting_single_isBigO_log' [ProperSpace E] {e : E} :
-    logCounting (single e) =O[atTop] log := by
-  rw [isBigO_iff]
-  use 2
-  rw [eventually_atTop]
-  use max ‖e‖ (exp |log ‖e‖|)
-  intro r hr
-  have h₁r : ‖e‖ ≤ r := le_of_max_le_left hr
-  have h₂r : exp |log ‖e‖| ≤ r := le_of_max_le_right hr
-  have h₃r : 1 ≤ r := by
-    linarith [h₂r, one_le_exp (abs_nonneg (log ‖e‖))]
-  rw [logCounting_single h₁r]
-  calc ‖log r - log ‖e‖‖
-    _ ≤ ‖log r‖ + ‖log ‖e‖‖ := by
-      apply norm_sub_le
-    _ ≤ 2 * ‖log r‖ := by
-      simpa [two_mul, norm_of_nonneg (log_nonneg h₃r)] using
-        (log_le_log (exp_pos _) h₂r)
+  · intro a ha b hb hab
+    apply logCounting_mono _ _ ((norm_nonneg e).trans_lt hb) hab
+    · simp [hD]
+    · simpa [mem_Ioi] using (norm_nonneg e).trans_lt ha
 
 lemma one_isLittleO_logCounting_single [ProperSpace E] {e : E} :
     (1 : ℝ → ℝ) =o[atTop] logCounting (single e) := by
   rw [isLittleO_iff]
   intro c hc
-  simp
+  simp only [Pi.one_apply, one_mem, CStarRing.norm_of_mem_unitary, norm_eq_abs, eventually_atTop,
+    ge_iff_le]
   use exp (|log ‖e‖| + c⁻¹)
   intro b hb
   have h₁b : 1 ≤ b := by
-    trans rexp (|log ‖e‖| + c⁻¹)
-    · apply one_le_exp
-      apply add_nonneg
-      · exact abs_nonneg (log ‖e‖)
-      · exact (inv_pos.2 hc).le
-    · exact hb
-  rw [← inv_mul_le_iff₀ hc, mul_one]
-  rw [abs_of_nonneg (logCounting_nonneg single_nonneg h₁b)]
-
-  trans logCounting (single e) (rexp (|log ‖e‖| + c⁻¹))
-  · rw [logCounting_single]
-    rw [le_sub_iff_add_le']
-    simp [le_abs_self (log ‖e‖)]
-    --
-    trans rexp (log ‖e‖)
-    · by_cases he : ‖e‖ = 0
-      · simp [he]
-      rw [exp_log]
-      aesop
-    · apply exp_monotone
-      nth_rw 1 [← add_zero (a := log ‖e‖)]
-      gcongr
-      · exact le_abs_self (log ‖e‖)
-      · exact (inv_pos.2 hc).le
-  · apply logCounting_mono single_nonneg
-    · simp [exp_pos]
-    · simp
-      apply one_pos.trans_le h₁b
-    exact hb
+    calc 1
+      _ ≤ exp (|log ‖e‖| + c⁻¹) :=
+        one_le_exp (add_nonneg (abs_nonneg (log ‖e‖)) (inv_pos.2 hc).le)
+      _ ≤ b := hb
+  have h₁c : ‖e‖ ≤ exp (|log ‖e‖| + c⁻¹) := by
+    calc ‖e‖
+      _ ≤ exp (log ‖e‖) := by
+        by_cases he : ‖e‖ = 0
+        · simp [he]
+        · simp_all [exp_log]
+      _ ≤ exp (|log ‖e‖| + c⁻¹) :=
+        exp_monotone (le_add_of_le_of_nonneg (le_abs_self _) (inv_pos.2 hc).le)
+  rw [← inv_mul_le_iff₀ hc, mul_one, abs_of_nonneg (logCounting_nonneg single_pos.le h₁b)]
+  calc c⁻¹
+    _ ≤ logCounting (single e) (exp (|log ‖e‖| + c⁻¹)) := by
+      simp [logCounting_single h₁c, le_sub_iff_add_le', le_abs_self (log ‖e‖)]
+    _ ≤ logCounting (single e) b := by
+      apply logCounting_mono single_pos.le (mem_Ioi.2 (exp_pos _)) _ hb
+      simpa [mem_Ioi] using one_pos.trans_le h₁b
 
 lemma zero_iff_logCounting_bounded [ProperSpace E] {D : locallyFinsuppWithin (univ : Set E) ℤ}
     (h : 0 ≤ D) :
@@ -170,59 +115,67 @@ lemma zero_iff_logCounting_bounded [ProperSpace E] {D : locallyFinsuppWithin (un
     simp [isBigO_of_le' (c := 0), h₂]
   · contrapose
     intro h₁
-    obtain ⟨e, he⟩ := xx h h₁
-
-    have := one_isLittleO_logCounting_single (e := e)
-    rw [isLittleO_iff] at this
-    simp at this
-
+    obtain ⟨e, he⟩ := le_single (lt_of_le_of_ne h (h₁ ·.symm))
     rw [isBigO_iff'']
     push_neg
     intro a ha
-    simp
-    rw [frequently_atTop]
-
+    simp only [Pi.one_apply, one_mem, CStarRing.norm_of_mem_unitary, norm_eq_abs, frequently_atTop,
+      ge_iff_le]
     intro b
-
-    have := this ha
-    obtain ⟨c, hc⟩ := this
+    obtain ⟨c, hc⟩ := eventually_atTop.1
+      (isLittleO_iff.1 (one_isLittleO_logCounting_single (e := e)) ha)
     let ℓ := 1 + max ‖e‖ (max |b| |c|)
     have h₁ℓ : b < ℓ := by
       calc b
-        _ ≤ |b| := le_abs_self b
-        _ ≤ max |b| |c| := le_max_left |b| |c|
-        _ ≤ max ‖e‖ (max |b| |c|) := le_max_right ‖e‖ (max |b| |c|)
-        _ < 1 + max ‖e‖ (max |b| |c|) := lt_one_add (max ‖e‖ (max |b| |c|))
+        _ ≤ |b| := le_abs_self _
+        _ ≤ max |b| |c| := le_max_left _ _
+        _ ≤ max ‖e‖ (max |b| |c|) := le_max_right _ _
+        _ < 1 + max ‖e‖ (max |b| |c|) := lt_one_add _
         _ = ℓ := rfl
     have h₂ℓ : c < ℓ := by
       calc c
-        _ ≤ |c| := le_abs_self c
-        _ ≤ max |b| |c| := le_max_right |b| |c|
-        _ ≤ max ‖e‖ (max |b| |c|) := le_max_right ‖e‖ (max |b| |c|)
-        _ < 1 + max ‖e‖ (max |b| |c|) := lt_one_add (max ‖e‖ (max |b| |c|))
+        _ ≤ |c| := le_abs_self _
+        _ ≤ max |b| |c| := le_max_right _ _
+        _ ≤ max ‖e‖ (max |b| |c|) := le_max_right _ _
+        _ < 1 + max ‖e‖ (max |b| |c|) := lt_one_add _
         _ = ℓ := rfl
-    have h₃ℓ : 1 ≤ ℓ := by
-      simp [ℓ]
+    have h₃ℓ : 1 ≤ ℓ := by simp [ℓ]
     have h₄ℓ : ℓ > ‖e‖ := by
       calc ‖e‖
-        _ ≤ max ‖e‖ (max |b| |c|) := le_max_left ‖e‖ (max |b| |c|)
-        _ < 1 + max ‖e‖ (max |b| |c|) := lt_one_add (max ‖e‖ (max |b| |c|))
+        _ ≤ max ‖e‖ (max |b| |c|) := le_max_left _ _
+        _ < 1 + max ‖e‖ (max |b| |c|) := lt_one_add _
         _ = ℓ := rfl
     use 1 + ℓ, h₁ℓ.le.trans (lt_one_add ℓ).le
     calc 1
-      _ ≤ (a * |logCounting (single e) ℓ|) := hc ℓ h₂ℓ.le
+      _ ≤ (a * |logCounting (single e) ℓ|) := by simpa [h₂ℓ.le] using hc ℓ
       _ ≤ (a * |logCounting D ℓ|) := by
         gcongr
-        · apply logCounting_nonneg single_nonneg h₃ℓ
+        · apply logCounting_nonneg single_pos.le h₃ℓ
         · apply logCounting_le he h₃ℓ
       _ < a * |logCounting D (1 + ℓ)| := by
         gcongr 2
-        rw [abs_of_nonneg (logCounting_nonneg h h₃ℓ)]
-        rw [abs_of_nonneg (logCounting_nonneg h (le_add_of_nonneg_right (zero_le_one.trans h₃ℓ)))]
-        apply logCounting_strictMono he
-        simp [h₄ℓ]
-        simp
-        exact h₄ℓ.trans (lt_one_add ℓ)
-        exact lt_one_add ℓ
+        rw [abs_of_nonneg (logCounting_nonneg h h₃ℓ),
+          abs_of_nonneg (logCounting_nonneg h (le_add_of_nonneg_right (zero_le_one.trans h₃ℓ)))]
+        apply logCounting_strictMono he h₄ℓ (h₄ℓ.trans (lt_one_add ℓ)) (lt_one_add ℓ)
 
 end Function.locallyFinsuppWithin
+
+namespace ValueDistribution
+
+theorem logCounting_isBigO_one_iff_analyticOnNhd {f : ℂ → ℂ} (h : Meromorphic f) :
+    logCounting f ⊤ =O[atTop] (1 : ℝ → ℝ) ↔ AnalyticOnNhd ℂ (toMeromorphicNFOn f ⊤) ⊤ := by
+  simp only [logCounting, reduceDIte, top_eq_univ]
+  rw [← Function.locallyFinsuppWithin.zero_iff_logCounting_bounded (negPart_nonneg _)]
+  constructor
+  · intro h₁f z hz
+    apply (meromorphicNFOn_toMeromorphicNFOn f ⊤
+      trivial).meromorphicOrderAt_nonneg_iff_analyticAt.1
+    rw [meromorphicOrderAt_toMeromorphicNFOn h.meromorphicOn (by trivial), ← WithTop.untop₀_nonneg,
+      ← h.meromorphicOn.divisor_apply (by trivial), ← negPart_eq_zero,
+      ← locallyFinsuppWithin.negPart_apply]
+    aesop
+  · intro h₁f
+    rwa [negPart_eq_zero, ← h.meromorphicOn.divisor_of_toMeromorphicNFOn,
+      (meromorphicNFOn_toMeromorphicNFOn _ _).divisor_nonneg_iff_analyticOnNhd]
+
+end ValueDistribution
