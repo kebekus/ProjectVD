ca1802426c049c3ec523884eaa35962acc088d1d
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-01-22 07:24:40 +0100

    Update BoundednessProximity.lean

--- VD/BoundednessProximity.lean
+++ VD/BoundednessProximity.lean
@@ -1,7 +1,7 @@
 import VD.Cartan
 import Mathlib
 
-open Function Metric Real Set Classical Topology ValueDistribution
+open Filter Function Metric Real Set Classical Topology ValueDistribution
 
 /-
 Prove that the proximity function of an analytic function `f` is bounded if and
@@ -14,7 +14,12 @@ variable
   {E : Type*} [NormedAddCommGroup E] [ProperSpace E]
 
 lemma proximity_bounded_iff_constant {f : ℂ → ℂ} (h : AnalyticOnNhd ℂ f Set.univ) :
-    ∃ c, f = c ↔ proximity f ⊤ =O[Filter.atTop] (1 : ℝ → ℝ) := by
-  sorry
+    (∃ c, f =ᶠ[codiscrete ℂ] c) ↔ proximity f ⊤ =O[atTop] (1 : ℝ → ℝ) := by
+  constructor
+  · intro h
+    obtain ⟨c, hc⟩ := h
+
+    sorry
+  · sorry
 
 end ValueDistribution
