391827ace7e10287e508f7cbae09b0cf8fe9537c
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-01-27 20:02:06 +0100

    Update ComplexMeanvalue.2.lean

--- VD/ComplexMeanvalue.2.lean
+++ VD/ComplexMeanvalue.2.lean
@@ -9,7 +9,8 @@ variable
 
 theorem circleAverage_of_differentiable_on₂ (hf : ∀ z ∈ closedBall c |R|, DifferentiableAt ℂ f z)
     (hw : w ∈ ball c |R|) (h₁w : w ≠ c) (hR : 0 < R) :
-    circleAverage (fun z ↦ ((z - c) * (z - w)⁻¹) • f z) c R = f w :=
+    circleAverage (fun z ↦ ((z - c) * (z - w)⁻¹) • f z) c R = f w := by
+
   let W := R * R / (conj (w - c))
   have : R < ‖W‖ := by
     unfold W
@@ -21,7 +22,8 @@ theorem circleAverage_of_differentiable_on₂ (hf : ∀ z ∈ closedBall c |R|, 
     constructor
     · simp [h₁w, sub_eq_zero]
     · exact hw
-  have : ∀ z ∈ closedBall c |R|, DifferentiableAt ℂ (fun x ↦ (x - (W + c))⁻¹ • f x) z := by
+
+  have τ₂ : ∀ z ∈ closedBall c |R|, DifferentiableAt ℂ (fun x ↦ (x - (W + c))⁻¹ • f x) z := by
     intro z hz
     apply DifferentiableAt.fun_smul _ (hf z hz)
     · apply DifferentiableAt.fun_inv _ _
@@ -31,9 +33,25 @@ theorem circleAverage_of_differentiable_on₂ (hf : ∀ z ∈ closedBall c |R|, 
         subst h
         simp at hz
         grind
-  have : circleAverage (fun x ↦ (x - (W + c))⁻¹ • f x) c R = 0 := by
-    --apply DiffContOnCl.circleIntegral_eq_zero
+
+  have τ₁ : DiffContOnCl ℂ (fun x ↦ (x - c)⁻¹ • (x - (W + c))⁻¹ • f x) (ball c R) := by
     sorry
+
+  have τ₀ : circleAverage (fun x ↦ (x - (W + c))⁻¹ • f x) c R = 0 := by
+    rw [circleAverage_eq_circleIntegral (ne_of_lt hR).symm]
+    rw [τ₁.circleIntegral_eq_zero hR.le]
+    simp
+
+  rw [← zero_add (a := circleAverage (fun z ↦ ((z - c) * (z - w)⁻¹) • f z) c R)]
+  rw [← τ₀]
+  rw [← circleAverage_fun_add]
+  simp_rw [← add_smul]
+
+
+
+  /-
   circleAverage_of_differentiable_on_off_countable₁ countable_empty
     (fun x hx ↦ (hf x hx).continuousAt.continuousWithinAt)
     (fun z hz ↦ hf z (by simp_all [le_of_lt])) hw hR
+  -/
+  sorry
