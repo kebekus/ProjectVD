bfa356d1ed9d95bc96e56daaf8ac87e54ce487bc
Matteo CIpollina <matteo.cipollina@yahoo.it>
2026-04-18 14:16:10 +0200

    Update CharacteristicMonotone.lean

--- VD/CharacteristicMonotone.lean
+++ VD/CharacteristicMonotone.lean
@@ -8,19 +8,18 @@ namespace ValueDistribution
 The characteristic function is monotone.
 
 Note: Given that the proximity function is not monotone in general, this is a
-surprisingly non-trivial consequence of Cartan's theorem,
-`characteristic_top_eq_circleAverage_logCounting_add_const`.
+surprisingly non-trivial consequence of Cartan's formula.
 -/
 theorem characteristic_monotoneOn {f : ℂ → ℂ} (h : Meromorphic f) :
     MonotoneOn (characteristic f ⊤) (Ioi 0) := by
   intro a ha b hb hab
-  obtain ⟨c, hc⟩ := characteristic_top_eq_circleAverage_logCounting_add_const h
-  rw [hc a, hc b]
+  rw [characteristic_top_eq_circleAverage_logCounting_add_circleAverage_log_trailingCoeff
+      ha.ne' h]
+  rw [characteristic_top_eq_circleAverage_logCounting_add_circleAverage_log_trailingCoeff
+      hb.ne' h]
   gcongr
-  · apply logCounting_circleIntegrable h
-  · apply logCounting_circleIntegrable h
+  · apply circleIntegrable_logCounting h
+  · apply circleIntegrable_logCounting h
   · apply logCounting_monotoneOn ha hb hab
-  · aesop
-  · aesop
 
 end ValueDistribution
