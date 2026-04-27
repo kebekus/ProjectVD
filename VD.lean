2e1cf856911d8830bbb74ccc5f4fce08888af26e
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-04-27 17:06:04 +0200

    Update VD.lean

--- VD.lean
+++ VD.lean
@@ -1,12 +1,12 @@
 import VD.BoundednessCharacteristic
 import VD.BoundednessProximity
 import VD.Cartan
-import VD.CartanTrailing
 import VD.CharacteristicMonotone
 import VD.LinearDiffOp.Basic
 import VD.LinearDiffOp.Regularity
 import VD.MathlibPending.BlaschkeDecomp2
 import VD.MathlibSubmitted.BlaschkeDecomp
+import VD.MathlibSubmitted.Cartan
 import VD.MathlibSubmitted.Poisson_log_affine
 import VD.MathlibSubmitted.ProximityIntegral
 import VD.PoissonJensen
