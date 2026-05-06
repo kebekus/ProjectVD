b1c2a92930b568a9fd2e072c8608b3eb8ede59a5
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-05-06 16:10:23 +0200

    Update

--- VD/MathlibSubmitted/Trivia.lean
+++ VD/MathlibSubmitted/Trivia.lean
@@ -25,10 +25,3 @@ theorem MeromorphicOn.codiscreteWithin_setOf_ne_zero {U : Set ℂ} {f : ℂ → 
     h₁f.codiscreteWithin_setOf_meromorphicOrderAt_eq_zero_or_top h₂f] with x h₁x h₂x
   have := h₂f x h₂x.1
   simp_all [← h₁x.analyticOrderAt_eq_zero, h₁x.meromorphicOrderAt_eq]
-
-theorem finprod_ne_zero {ι : Type*} {M₀ : Type*} [CommMonoidWithZero M₀] [Nontrivial M₀] [NoZeroDivisors M₀]
-  {f : ι → M₀} (h : ∀ i, f i ≠ 0) :
-    ∏ᶠ i, f i ≠ 0 := by
-  by_cases h₂ : Set.Finite f.mulSupport
-  · grind [finprod_eq_prod f h₂, Finset.prod_ne_zero_iff]
-  · simp [finprod_of_infinite_mulSupport h₂]
