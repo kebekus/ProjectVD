79e23c07f22f4ac4b42924109cd5fb183786a70c
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-06-04 19:43:45 +0200

    Working…

--- VD.lean
+++ VD.lean
@@ -2,9 +2,9 @@ import VD.BoundednessCharacteristic
 import VD.BoundednessProximity
 import VD.LinearDiffOp.Basic
 import VD.LinearDiffOp.Regularity
-import VD.MathlibPending.BlaschkeDecomp2
 import VD.MathlibPending.BlaschkeDecomp3
 import VD.MathlibPending.Cartan
+import VD.MathlibSubmitted.BlaschkeDecomp2
 import VD.MathlibSubmitted.ProximityIntegral
 import VD.PoissonJensen0
 import VD.PoissonJensen1
--- VD/PoissonJensen1.lean
+++ VD/PoissonJensen1.lean
@@ -6,6 +6,44 @@ open Complex Filter Function MeromorphicOn Metric Real Set Classical Topology --
 ## Additional Material
 -/
 
+variable
+  {𝕜 : Type*} [NontriviallyNormedField 𝕜]
+  {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+  {x : 𝕜}
+
+@[simp] theorem meromorphicAt_fun_comp_add_const_iff_meromorphicAt {c : 𝕜} {f : 𝕜 → E} :
+    MeromorphicAt (f ∘ fun z ↦ (z + c)) (x - c) ↔ MeromorphicAt f x := by
+  constructor
+  · intro h
+    convert h.comp_analyticAt (g := fun z ↦ z - c) (by fun_prop)
+    aesop
+  · intro h
+    rw [(by ring : x = (x - c) + c)] at h
+    exact h.comp_analyticAt (g := fun z ↦ z + c) (by fun_prop)
+
+@[simp] theorem meromorphicAt_comp_add_const_iff_meromorphicAt {c : 𝕜} {f : 𝕜 → E} :
+    MeromorphicAt (fun z ↦ f (z + c)) (x - c) ↔ MeromorphicAt f x :=
+  meromorphicAt_fun_comp_add_const_iff_meromorphicAt
+
+@[simp] theorem meromorphicAt_fun_comp_sub_const_iff_meromorphicAt {c : 𝕜} {f : 𝕜 → E} :
+    MeromorphicAt (f ∘ fun z ↦ (z - c)) (x + c) ↔ MeromorphicAt f x := by
+  simp [← meromorphicAt_fun_comp_add_const_iff_meromorphicAt (f := f) (c := -c), sub_eq_add_neg]
+
+@[simp] theorem meromorphicAt_comp_sub_const_iff_meromorphicAt {c : 𝕜} {f : 𝕜 → E} :
+    MeromorphicAt (fun z ↦ f (z - c)) (x + c) ↔ MeromorphicAt f x :=
+  meromorphicAt_fun_comp_sub_const_iff_meromorphicAt
+
+@[simp] theorem meromorphicOn_fun_comp_add_const_iff_meromorphicOn {c : 𝕜} {U : Set 𝕜} {f : 𝕜 → E} :
+    MeromorphicOn (f ∘ fun z ↦ (z + c)) {x | x + c ∈ U} ↔ MeromorphicOn f U := by
+  constructor
+  · intro h y hy
+    rw [← meromorphicAt_fun_comp_add_const_iff_meromorphicAt (c := c)]
+    apply h
+    aesop
+  · intro h y hy
+    rw [← meromorphicAt_fun_comp_sub_const_iff_meromorphicAt (c := c),
+      (by aesop : ((f ∘ fun z ↦ z + c) ∘ fun z ↦ z - c) = f)]
+    exact h (y + c) (mem_preimage.mp hy)
 
 /-!
 ## Formula goes here
@@ -24,7 +62,7 @@ theorem poissonJensen {c : ℂ}
     (hR : 0 < R) : -- can be decuced
     Real.log ‖meromorphicTrailingCoeffAt f w‖
       = circleAverage (re ∘ herglotzRieszKernel c w * (Real.log ‖f ·‖)) c R
-        - ∑ᶠ i, (divisor f (ball c R) i) * Real.log ‖canonicalFactor R (i - c) w‖ := by
+        - ∑ᶠ i, (divisor f (ball c R) i) * Real.log ‖canonicalFactor R i (w - c)‖ := by
   let g := fun z ↦ f (z + c)
   have : f = fun z ↦ g (z - c) := by simp [g]
   repeat rw [this]
@@ -34,8 +72,18 @@ theorem poissonJensen {c : ℂ}
   rw [this]
   rw [poissonJensen₀ (R := R)]
   congr 1
-  · sorry
-  · sorry
+  · simp only [← Real.circleAverage_map_add_const (c := c), Pi.mul_apply, comp_apply,
+      add_sub_cancel_right]
+    congr
+    ext x
+    exact (herglotzRieszKernel_add_const c w x).symm
+  · apply finsum_congr
+    intro x
+    congr 2
+    -- should be simp
+    rw [divisor_def, divisor_def]
+    grind
+    sorry
   · sorry
   · sorry
   · sorry
