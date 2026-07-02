71021566a52a1ae23b510fa6cfc66ae16f34d02b
Stefan Kebekus <kebekus@users.noreply.github.com>
2026-07-02 19:44:00 +0200

    Saving

--- VD/CharacteristicMoebius.lean
+++ VD/CharacteristicMoebius.lean
@@ -6,6 +6,7 @@ Authors: Stefan Kebekus
 import Mathlib.Analysis.Complex.ValueDistribution.FirstMainTheorem
 import Mathlib.Analysis.Meromorphic.IsolatedZeros
 import VD.MathlibPending.BoundednessCharacteristic
+import VD.MathlibSubmitted.Scaling
 
 /-!
 # Invariance of the Characteristic Function under Automorphisms of the Projective Line
@@ -38,74 +39,79 @@ variable {f : ℂ → ℂ}
 ## Scaling by a Nonzero Constant
 -/
 
+/-!
+### LogCounting
+-/
+
 variable
   {𝕜 : Type*} [NontriviallyNormedField 𝕜] {U : Set 𝕜} {z : 𝕜}
   {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
 
-@[simp] theorem meromorphicAt_const_smul_iff_meromorphicAt {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
-    MeromorphicAt (s • f) z ↔ MeromorphicAt f z := by
-  constructor
-  <;> intro hf
-  · rw [((eq_inv_smul_iff₀ hs).mpr rfl : f = s⁻¹ • s • f)]
-    fun_prop
-  · fun_prop
-
-@[simp] theorem meromorphicAt_fun_const_smul_iff_meromorphicAt {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
-    MeromorphicAt (fun x ↦ s • f x) z ↔ MeromorphicAt f z :=
-  meromorphicAt_const_smul_iff_meromorphicAt hs
-
-@[simp] theorem meromorphicOn_const_smul_iff_meromorphicOn {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
-    MeromorphicOn (s • f) U ↔ MeromorphicOn f U :=
-  ⟨fun hf x hx ↦ (meromorphicAt_fun_const_smul_iff_meromorphicAt hs).mp (hf x hx),
-    fun hf x hx ↦ (meromorphicAt_const_smul_iff_meromorphicAt hs).mpr (hf x hx)⟩
-
-@[simp] theorem meromorphicOn_fun_const_smul_iff_meromorphicOn {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
-    MeromorphicOn (fun x ↦ s • f x) U ↔ MeromorphicOn f U :=
-  meromorphicOn_const_smul_iff_meromorphicOn hs
-
-@[simp] theorem meromorphicOrderAt_const_smul_iff_meromorphicOrderAt {f : 𝕜 → E} {s : 𝕜}
-    (hs : s ≠ 0) :
-    meromorphicOrderAt (s • f) z = meromorphicOrderAt f z := by
-  by_cases hf : MeromorphicAt f z
-  · rw [(by aesop : s • f = (fun (_ : 𝕜) ↦ s) • f),
-      meromorphicOrderAt_smul_of_ne_zero (by fun_prop) hs]
-  simp_all
-
-@[simp] theorem meromorphicOrderAt_fun_const_smul_iff_meromorphicOrderAt {f : 𝕜 → E} {s : 𝕜}
-    (hs : s ≠ 0) :
-    meromorphicOrderAt (fun x ↦ s • f x) z = meromorphicOrderAt f z :=
-  meromorphicOrderAt_const_smul_iff_meromorphicOrderAt hs
-
-@[simp] theorem divisor_const_smul {f : 𝕜 → E} {s : 𝕜} {U : Set 𝕜} (hs : s ≠ 0) :
-    divisor (s • f) U = divisor f U := by
-  ext z
-  by_cases h₁f : MeromorphicOn f U
-  · by_cases hz : z ∈ U
-    · rw [divisor_apply h₁f hz, divisor_apply (by simp_all) hz]
-      simp_all
-    · simp_all
-  · simp_all
-
-@[simp] theorem divisor_fun_const_smul {f : 𝕜 → E} {s : 𝕜} {U : Set 𝕜} (hs : s ≠ 0) :
-    divisor (fun x ↦ s • f x) U = divisor f U :=
-  divisor_const_smul hs
-
+/-- The logCounting function at top is invariant under scaling. -/
 @[simp] theorem logCounting_const_smul_top [ProperSpace 𝕜] {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
     ValueDistribution.logCounting (s • f) ⊤ = ValueDistribution.logCounting f ⊤ := by
   simp_all [logCounting_top]
 
+/-- The logCounting function at top is invariant under scaling. -/
 @[simp] theorem logCounting_fun_const_smul_top [ProperSpace 𝕜] {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
     ValueDistribution.logCounting (fun x ↦ s • f x) ⊤ = ValueDistribution.logCounting f ⊤ :=
   logCounting_const_smul_top hs
 
+/-!
+### Proximity
+-/
+
 variable
   {E : Type*} [NormedAddCommGroup E] [NormedSpace ℂ E]
+
+/-- If `f` is circle integrable, then so are its scalar multiples. -/
+theorem CircleIntegrable.const_smul' {f : ℂ → E} {c s : ℂ} {R : ℝ} (h : CircleIntegrable f c R) :
+    CircleIntegrable (s • f) c R := by
+  sorry
+
+@[simp]
+theorem circleIntegrable_iff_circleIntegrable_const_smul {f : ℂ → E} {c s : ℂ} {R : ℝ} (h : s ≠ 0) :
+    CircleIntegrable (s • f) c R ↔ CircleIntegrable f c R := by
+  constructor <;> intro hf
+  · sorry
+  · sorry
+
+
+
+/--
+The proximity function `f • g` at `⊤` is less than or equal to the sum of the proximity functions of
+`f` and `g`, respectively.
+-/
+theorem proximity_smul_top_le {f₁ : ℂ → ℂ} {f₂ : ℂ → E} (h₁f₁ : Meromorphic f₁)
+    (h₁f₂ : Meromorphic f₂) :
+    proximity (f₁ • f₂) ⊤ ≤ proximity f₁ ⊤ + proximity f₂ ⊤ := by
+  calc proximity (f₁ • f₂) ⊤
+    _ = circleAverage (fun x ↦ log⁺ (‖f₁ x‖ * ‖f₂ x‖)) 0 := by
+      simp [proximity, norm_smul]
+    _ ≤ circleAverage (fun x ↦ log⁺ ‖f₁ x‖ + log⁺ ‖f₂ x‖) 0 := by
+      intro r
+      apply circleAverage_mono
+      · simp_rw [← norm_smul]
+        apply MeromorphicOn.circleIntegrable_posLog_norm
+        apply Meromorphic.meromorphicOn
+        fun_prop
+      · apply (MeromorphicOn.circleIntegrable_posLog_norm (fun x a ↦ h₁f₁ x)).add
+          (MeromorphicOn.circleIntegrable_posLog_norm (fun x a ↦ h₁f₂ x))
+      · exact fun _ _ ↦ posLog_mul
+    _ = circleAverage (log⁺ ‖f₁ ·‖) 0 + circleAverage (log⁺ ‖f₂ ·‖) 0 := by
+      ext r
+      apply circleAverage_add
+      · exact MeromorphicOn.circleIntegrable_posLog_norm (fun x a ↦ h₁f₁ x)
+      · exact MeromorphicOn.circleIntegrable_posLog_norm (fun x a ↦ h₁f₂ x)
+    _ = proximity f₁ ⊤ + proximity f₂ ⊤ := by simp [proximity]
 
 theorem isBigO_proximity_top_sub_proximity_const_smul_top [ProperSpace 𝕜] {f : ℂ → E} {s : ℂ}
     (hs : s ≠ 0) :
     (proximity (s • f) ⊤ - proximity f ⊤) =O[atTop] (1 : ℝ → ℝ) := by
   have η₁ {r : ℝ} : proximity (s • f) ⊤ r ≤ log⁺ ‖s‖ + proximity f ⊤ r := by
     rw [show (s • f) = (fun _ : ℂ ↦ s) • f by funext z; rfl]
+    have := proximity_smul_top_le
+    simpa [proximity_const] -- using (proximity_mul_top_le (Meromorphic.const l) hg) r
     sorry
   apply isBigO_of_le' (c := log⁺ ‖s‖ + log⁺ ‖s⁻¹‖)
   sorry
---
+++ VD/MathlibPending/Scaling.lean
@@ -0,0 +1,131 @@
+/-
+Copyright (c) 2026 Stefan Kebekus. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Stefan Kebekus
+-/
+import Mathlib.Analysis.Complex.ValueDistribution.FirstMainTheorem
+import Mathlib.Analysis.Meromorphic.IsolatedZeros
+import VD.MathlibPending.BoundednessCharacteristic
+
+
+open Asymptotics Filter Function Metric MeromorphicOn Real Set Topology ValueDistribution
+
+/-!
+## Scaling by a Nonzero Constant
+-/
+
+variable
+  {𝕜 : Type*} [NontriviallyNormedField 𝕜] {U : Set 𝕜} {z : 𝕜}
+  {E : Type*} [NormedAddCommGroup E] [NormedSpace 𝕜 E]
+
+/-- MeromorphicAt is invariant under scaling. -/
+@[simp] theorem meromorphicAt_const_smul_iff_meromorphicAt {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    MeromorphicAt (s • f) z ↔ MeromorphicAt f z := by
+  constructor
+  <;> intro hf
+  · rw [((eq_inv_smul_iff₀ hs).mpr rfl : f = s⁻¹ • s • f)]
+    fun_prop
+  · fun_prop
+
+/-- MeromorphicAt is invariant under scaling. -/
+@[simp] theorem meromorphicAt_fun_const_smul_iff_meromorphicAt {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    MeromorphicAt (fun x ↦ s • f x) z ↔ MeromorphicAt f z :=
+  meromorphicAt_const_smul_iff_meromorphicAt hs
+
+/-- MeromorphicOn is invariant under scaling. -/
+@[simp] theorem meromorphicOn_const_smul_iff_meromorphicOn {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    MeromorphicOn (s • f) U ↔ MeromorphicOn f U :=
+  ⟨fun hf x hx ↦ (meromorphicAt_const_smul_iff_meromorphicAt hs).mp (hf x hx),
+    fun hf x hx ↦ (meromorphicAt_const_smul_iff_meromorphicAt hs).mpr (hf x hx)⟩
+
+/-- MeromorphicOn is invariant under scaling. -/
+@[simp] theorem meromorphicOn_fun_const_smul_iff_meromorphicOn {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    MeromorphicOn (fun x ↦ s • f x) U ↔ MeromorphicOn f U :=
+  meromorphicOn_const_smul_iff_meromorphicOn hs
+
+/-- Meromorphic is invariant under scaling. -/
+@[simp] theorem meromorphic_const_smul_iff_meromorphic {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    Meromorphic (s • f) ↔ Meromorphic f :=
+  ⟨fun hf x ↦ (meromorphicAt_const_smul_iff_meromorphicAt hs).mp (hf x),
+    fun hf x ↦ (meromorphicAt_const_smul_iff_meromorphicAt hs).mpr (hf x)⟩
+
+/-- Meromorphic is invariant under scaling. -/
+@[simp] theorem meromorphic_fun_const_smul_iff_meromorphic {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    Meromorphic (fun x ↦ s • f x) ↔ Meromorphic f :=
+  meromorphic_const_smul_iff_meromorphic hs
+
+/-- meromorphicOrderAt is invariant under scaling. -/
+@[simp] theorem meromorphicOrderAt_const_smul_iff_meromorphicOrderAt {f : 𝕜 → E} {s : 𝕜}
+    (hs : s ≠ 0) :
+    meromorphicOrderAt (s • f) z = meromorphicOrderAt f z := by
+  by_cases hf : MeromorphicAt f z
+  · rw [(by aesop : s • f = (fun (_ : 𝕜) ↦ s) • f),
+      meromorphicOrderAt_smul_of_ne_zero (by fun_prop) hs]
+  simp_all
+
+/-- meromorphicOrderAt is invariant under scaling. -/
+@[simp] theorem meromorphicOrderAt_fun_const_smul_iff_meromorphicOrderAt {f : 𝕜 → E} {s : 𝕜}
+    (hs : s ≠ 0) :
+    meromorphicOrderAt (fun x ↦ s • f x) z = meromorphicOrderAt f z :=
+  meromorphicOrderAt_const_smul_iff_meromorphicOrderAt hs
+
+/-- The divisor of a function is invariant when scaling of the function. -/
+@[simp] theorem divisor_const_smul {f : 𝕜 → E} {s : 𝕜} {U : Set 𝕜} (hs : s ≠ 0) :
+    divisor (s • f) U = divisor f U := by
+  ext z
+  by_cases h₁f : MeromorphicOn f U
+  · by_cases hz : z ∈ U
+    · rw [divisor_apply h₁f hz, divisor_apply (by simp_all) hz]
+      simp_all
+    · simp_all
+  · simp_all only [ne_eq, not_false_eq_true, meromorphicOn_const_smul_iff_meromorphicOn,
+    divisor_eq_zero_of_not_meromorphicOn]
+
+/-- The divisor of a function is invariant when scaling of the function. -/
+@[simp] theorem divisor_fun_const_smul {f : 𝕜 → E} {s : 𝕜} {U : Set 𝕜} (hs : s ≠ 0) :
+    divisor (fun x ↦ s • f x) U = divisor f U :=
+  divisor_const_smul hs
+
+variable
+  {F : Type*} [NormedAddCommGroup F] [NormedSpace 𝕜 F]
+
+/-- Special case of `analyticAt_const`, required to make `fun_prop` work. -/
+@[fun_prop] theorem analyticAt_zero {G H : Type*} [NormedAddCommGroup G] [NormedSpace 𝕜 G]
+    [NormedAddCommGroup H] [NormedSpace 𝕜 H] {x : G} : AnalyticAt 𝕜 (0 : G → H) x :=
+  analyticAt_const
+
+attribute [fun_prop] AnalyticAt.meromorphicNFAt
+
+@[fun_prop] theorem MeromorphicNFAt.smul_const {f : 𝕜 → E} {s : 𝕜} (hf : MeromorphicNFAt f z) :
+    MeromorphicNFAt (s • f) z := by
+  by_cases h : s = 0
+  · rw [h, zero_smul]
+    fun_prop
+  exact hf.smul_analytic (by fun_prop) h
+
+/-- MeromorphicNFAt is invariant under scaling. -/
+@[simp] theorem meromorphicNFAt_const_smul_iff_meromorphicNFAt {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    MeromorphicNFAt (s • f) z ↔ MeromorphicNFAt f z := by
+  constructor
+  <;> intro hf
+  · rw [((eq_inv_smul_iff₀ hs).mpr rfl : f = s⁻¹ • s • f)]
+    fun_prop
+  · fun_prop
+
+/-- MeromorphicNFAt is invariant under scaling. -/
+@[simp] theorem meromorphicNFAt_fun_const_smul_iff_meromorphiNFcAt {f : 𝕜 → E} {s : 𝕜}
+    (hs : s ≠ 0) :
+    MeromorphicNFAt (fun x ↦ s • f x) z ↔ MeromorphicNFAt f z :=
+  meromorphicNFAt_const_smul_iff_meromorphicNFAt hs
+
+/-- MeromorphicNFOn is invariant under scaling. -/
+@[simp] theorem meromorphicNFOn_const_smul_iff_meromorphicNFOn {f : 𝕜 → E} {s : 𝕜} (hs : s ≠ 0) :
+    MeromorphicNFOn (s • f) U ↔ MeromorphicNFOn f U :=
+  ⟨fun hf _ hx ↦ (meromorphicNFAt_const_smul_iff_meromorphicNFAt hs).mp (hf hx),
+    fun hf _ hx ↦ (meromorphicNFAt_const_smul_iff_meromorphicNFAt hs).mpr (hf hx)⟩
+
+/-- MeromorphicNFOn is invariant under scaling. -/
+@[simp] theorem meromorphicNFOn_fun_const_smul_iff_meromorphicNFOn {f : 𝕜 → E} {s : 𝕜}
+    (hs : s ≠ 0) :
+    MeromorphicNFOn (fun x ↦ s • f x) U ↔ MeromorphicNFOn f U :=
+  meromorphicNFOn_const_smul_iff_meromorphicNFOn hs
